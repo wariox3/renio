64632bd31be261d5f4f859a5edc0d6158ec11e13
andres <afcanop@gmail.com>
2023-07-27 13:41:16 -0500

    feat: traducciones bases

--- src/app/comun/componentes/base-detalle/base-detalle.component.ts
+++ src/app/comun/componentes/base-detalle/base-detalle.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationModule } from '@modulos/i18n';
 
 @Component({
-    selector: 'app-comun-base-detalle',
-    standalone: true,
-    templateUrl: './base-detalle.component.html',
-    styleUrls: ['./base-detalle.component.scss'],
-    imports: [
-        CommonModule,
-    ]
+  selector: 'app-comun-base-detalle',
+  standalone: true,
+  templateUrl: './base-detalle.component.html',
+  styleUrls: ['./base-detalle.component.scss'],
+  imports: [CommonModule, RouterModule, TranslateModule, TranslationModule],
 })
 export class BaseDetalleComponent {
-  constructor(
-  ) {}
-
-
+  constructor() {}
 }
--- src/app/comun/componentes/base-nuevo/base-nuevo.component.ts
+++ src/app/comun/componentes/base-nuevo/base-nuevo.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationModule } from '@modulos/i18n';
 
 @Component({
   selector: 'app-comun-base-nuevo',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
+  imports: [CommonModule, RouterModule, TranslateModule, TranslationModule],
   templateUrl: './base-nuevo.component.html',
-  styleUrls: ['./base-nuevo.component.scss']
+  styleUrls: ['./base-nuevo.component.scss'],
 })
 export class BaseNuevoComponent {
-
-  constructor(
-  ) {}
-
-
+  constructor() {}
 }
