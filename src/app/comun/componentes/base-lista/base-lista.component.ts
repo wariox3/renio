618ef3f4ec3e443a5eaebf0cc4609ca501f7f382
andres <afcanop@gmail.com>
2023-07-27 14:04:04 -0500

    feat: traducciones

--- src/app/comun/componentes/base-lista/base-lista.component.ts
+++ src/app/comun/componentes/base-lista/base-lista.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationModule } from '@modulos/i18n';
 
 
 @Component({
   selector: 'app-comun-base-lista',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, TranslateModule, TranslationModule],
   templateUrl: './base-lista.component.html',
   styleUrls: ['./base-lista.component.scss']
 })
