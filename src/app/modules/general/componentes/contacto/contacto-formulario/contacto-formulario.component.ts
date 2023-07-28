ecf465ee890c4d72fac3af2a4d92448800bd3dd5
sebas957 <baxtian957@hotmail.com>
2023-07-28 11:53:12 -0500

    Varios

--- src/app/modules/general/componentes/contacto/contacto-formulario/contacto-formulario.component.ts
+++ src/app/modules/general/componentes/contacto/contacto-formulario/contacto-formulario.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { General } from '@comun/clases/general';
 import { ContactoFormulario } from '@interfaces/general/contacto';
+import { HttpService } from '@comun/services/http.service';
 
 @Component({
   selector: 'app-contacto-formulario',
@@ -12,12 +13,16 @@ export class ContactoFormularioComponent extends General implements OnInit {
   formularioContacto: FormGroup;
   @Input() informacionContacto!: ContactoFormulario;
 
-  constructor(private formBuilder: FormBuilder) {
+  arrIdentificacion: ContactoFormulario[] = [];
+
+  constructor(private formBuilder: FormBuilder, private httpService: HttpService) {
     super();
+
   }
 
   ngOnInit() {
     this.initForm();
+    this.obtenerTipoDocumento();
   }
 
   initForm() {
@@ -31,7 +36,7 @@ export class ContactoFormularioComponent extends General implements OnInit {
         Validators.compose([
           Validators.required,
           Validators.minLength(3),
-          Validators.maxLength(100),
+          Validators.maxLength(20),
         ]),
       ],
       nombre_corto: [
@@ -69,6 +74,7 @@ export class ContactoFormularioComponent extends General implements OnInit {
         this.informacionContacto.regimen,
         Validators.compose([Validators.minLength(1)]),
       ],
+      tipo_documento: [this.informacionContacto.identificacion, Validators.required],
     });
   }
 
@@ -83,7 +89,29 @@ export class ContactoFormularioComponent extends General implements OnInit {
     // }
   }
 
-  limpiarFormulario() {
-    this.formularioContacto.reset();
+  // limpiarFormulario() {
+  //   this.formularioContacto.reset();
+  // }
+
+  obtenerTipoDocumento() {
+    this.httpService.get<any>('general/identificacion/').subscribe((respuesta) => {
+      this.arrIdentificacion = respuesta;
+      this.changeDetectorRef.detectChanges();
+    });
+
+    // Simulando la respuesta de la API que proporcionaste
+    // const apiResponse = [
+    //   {
+    //     "id": 1,
+    //     "nombre": "Registro civil de nacimiento"
+    //   },
+    //   {
+    //     "id": 2,
+    //     "nombre": "Tarjeta de identidad"
+    //   },
+    // ];
+
+    // Asigna la lista de opciones a la variable
+
   }
 }
