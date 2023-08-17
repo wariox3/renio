8f43f7e9f6132542c9556719b1861ccd216087d4
andres <afcanop@gmail.com>
2023-08-17 16:46:14 -0500

    contacto

--- src/app/modules/general/componentes/contacto/contacto-detalle/contacto-detalle.component.ts
+++ src/app/modules/general/componentes/contacto/contacto-detalle/contacto-detalle.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { General } from '@comun/clases/general';
 
 @Component({
   selector: 'app-contacto-detalle',
   templateUrl: './contacto-detalle.component.html',
   styleUrls: ['./contacto-detalle.component.scss']
 })
-export default class ContactoDetalleComponent {
-
+export default class ContactoDetalleComponent extends General implements OnInit {
+  constructor() {
+    super();
+  }
+  ngOnInit() {
+;
+  }
 }
