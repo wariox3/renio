bd2ba3d595c16e35019dfb9b5c0fcd09db2b54b6
sebas957 <baxtian957@hotmail.com>
2023-09-04 14:42:27 -0500

    ajustes en contactos

--- src/app/modules/general/componentes/contacto/contacto-detalle/contacto-detalle.component.ts
+++ src/app/modules/general/componentes/contacto/contacto-detalle/contacto-detalle.component.ts
@@ -11,7 +11,7 @@ import {
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationModule } from '@modulos/i18n';
 import { HttpService } from '@comun/services/http.service';
-import { asyncScheduler, tap, throttleTime } from 'rxjs';
+import { asyncScheduler, tap, throttleTime, zip } from 'rxjs';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -31,6 +31,9 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 export default class ContactDetalleComponent extends General implements OnInit {
   formularioContacto: FormGroup;
   arrCiudades: any[];
+  arrIdentificacion: any[];
+  arrTipoPersona: any[];
+  arrRegimen: any[];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -41,13 +44,13 @@ export default class ContactDetalleComponent extends General implements OnInit {
 
   ngOnInit() {
     this.iniciarFormulario();
-    
+    this.consultarInformacion();
   }
 
   iniciarFormulario() {
     this.formularioContacto = this.formBuilder.group({
-      identificacion: ['', Validators.compose([Validators.required])],
-      numero_identificacion: [''],
+      numero_identificacion: ['', Validators.compose([Validators.required])],
+      identificacion: [''],
       nombre_corto: [''],
       direccion: [''],
       ciudad_nombre: [''],
@@ -97,12 +100,82 @@ export default class ContactDetalleComponent extends General implements OnInit {
       .subscribe();
   }
 
+  consultarInformacion() {
+    zip(
+      this.httpService.post<{ cantidad_registros: number; registros: any[] }>(
+        'general/funcionalidad/lista-autocompletar/',
+        {
+          filtros: [
+            {
+              id: '1692284537644-1688',
+              operador: '__contains',
+              propiedad: 'nombre__contains',
+              valor1: ``,
+              valor2: '',
+            },
+          ],
+          limite: 0,
+          desplazar: 0,
+          ordenamientos: [],
+          limite_conteo: 10000,
+          modelo: 'Identificacion',
+        }
+      ),
+      this.httpService.post<{ cantidad_registros: number; registros: any[] }>(
+        'general/funcionalidad/lista-autocompletar/',
+        {
+          filtros: [
+            {
+              id: '1692284537644-1688',
+              operador: '__contains',
+              propiedad: 'nombre__contains',
+              valor1: ``,
+              valor2: '',
+            },
+          ],
+          limite: 0,
+          desplazar: 0,
+          ordenamientos: [],
+          limite_conteo: 10000,
+          modelo: 'Regimen',
+        }
+      ),
+      this.httpService.post<{ cantidad_registros: number; registros: any[] }>(
+        'general/funcionalidad/lista-autocompletar/',
+        {
+          filtros: [
+            {
+              id: '1692284537644-1688',
+              operador: '__contains',
+              propiedad: 'nombre__contains',
+              valor1: ``,
+              valor2: '',
+            },
+          ],
+          limite: 0,
+          desplazar: 0,
+          ordenamientos: [],
+          limite_conteo: 10000,
+          modelo: 'TipoPersona',
+        }
+      )
+
+    ).subscribe((respuesta:any) => {
+      this.arrIdentificacion = respuesta[0].registros;
+      this.arrRegimen = respuesta[1].registros;
+      this.arrTipoPersona = respuesta[2].registros;
+      this.changeDetectorRef.detectChanges();
+    });
+  }
+
   modificarCampoFormulario(campo: string, dato: any) {
     this.formularioContacto?.markAsDirty();
     this.formularioContacto?.markAsTouched();
     if (campo === 'ciudad') {
       this.formularioContacto.get(campo)?.setValue(dato.ciudad_id);
-      this.formularioContacto.get('ciudad_nombre')?.setValue(dato.ciudad_nombre);
+      this.formularioContacto
+        .get('ciudad_nombre')
+        ?.setValue(dato.ciudad_nombre);
     }
     this.changeDetectorRef.detectChanges();
   }
