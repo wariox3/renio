57f0488d9a1b83a25d07494beca7d15dae5c8a42
andres <afcanop@gmail.com>
2023-09-14 11:12:27 -0500

    feat: correción enla clase de  errores

--- src/app/interceptores/errorhttp.interceptor.ts
+++ src/app/interceptores/errorhttp.interceptor.ts
@@ -4,57 +4,63 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpErrorResponse
+  HttpErrorResponse,
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { AlertaService } from '@comun/services/alerta.service';
 
-
 @Injectable()
 export class ErrorhttpInterceptor implements HttpInterceptor {
-
   constructor(private alertService: AlertaService) {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler){
-    return next.handle(request)
-      .pipe(
-        catchError((error: HttpErrorResponse)=>{
-          let errorCodigo: Number;
-          let errorMensaje: string;
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorCodigo: Number;
+        let errorMensaje: string = "";
 
-          if (error.error instanceof ErrorEvent) {
-            // Error de cliente
-            errorMensaje = `Error: ${error.error.message}`;
-          } else {
-            // Error del servidor
-            switch (error.status) {
-              case 404:
-                errorCodigo = 404
-                errorMensaje = 'El recurso solicitado no se encontró.';
-                break;
-              case 500:
-                errorCodigo = 500
-                errorMensaje = 'Se produjo un error interno en el servidor.';
-                break;
-              // Agrega más casos según tus necesidades
-              default:
-                let objError = error.error
-                if(objError.hasOwnProperty('error')){
-                  errorCodigo = objError.codigo;
-                  errorMensaje = objError.error;
+        if (error.error instanceof ErrorEvent) {
+          // Error de cliente
+          errorMensaje = `Error: ${error.error.message}`;
+        } else {
+          // Error del servidor
+          switch (error.status) {
+            case 404:
+              errorCodigo = 404;
+              errorMensaje = 'El recurso solicitado no se encontró.';
+              break;
+            case 500:
+              errorCodigo = 500;
+              errorMensaje = 'Se produjo un error interno en el servidor.';
+              break;
+            // Agrega más casos según tus necesidades
+            default:
+              let objError = error.error;
+              if (objError.hasOwnProperty('error')) {
+                errorCodigo = objError.codigo;
+                errorMensaje = objError.error;
+              }
+              if (objError.hasOwnProperty('mensaje')) {
+                errorCodigo = objError.codigo;
+                errorMensaje = objError.mensaje;
+              }
+              if (objError.hasOwnProperty('validaciones')) {
+                for (const key in objError.validaciones) {
+                  if (objError.validaciones.hasOwnProperty(key)) {
+                    const value = objError.validaciones[key];
+                    errorMensaje += `<br/> ${key}: ${value} <br/>`;
+                  }
                 }
-                if(objError.hasOwnProperty('mensaje')){
-                  errorCodigo = objError.codigo;
-                  errorMensaje = objError.mensaje
+              }
 
-                }
-
-                break;
-            }
+              break;
           }
+        }
 
-          return throwError(() => this.alertService.mensajeError(`Error ${errorCodigo}`, errorMensaje));
-        })
-      );
+        return throwError(() =>
+          this.alertService.mensajeError(`Error ${errorCodigo}`, errorMensaje)
+        );
+      })
+    );
   }
 }
