cd36569f1df872f9aed76c09300d7865541b4f2a
andres <afcanop@gmail.com>
2023-09-18 08:36:34 -0500

    fix: cambios en  detectar un subdominio

--- src/app/comun/clases/subdomino.ts
+++ src/app/comun/clases/subdomino.ts
@@ -1,24 +1,24 @@
-import { inject } from "@angular/core";
-import { environment } from "@env/environment";
-import { Store } from "@ngrx/store";
-import { obtenerEmpresaSubdominio } from "@redux/selectors/empresa.selectors";
-import { SubdominioService } from "@comun/services/subdominio.service";
+import { inject } from '@angular/core';
+import { environment } from '@env/environment';
+import { SubdominioService } from '@comun/services/subdominio.service';
 
 export class Subdomino {
-
   urlSubDominio: string = environment.URL_API_SUBDOMINIO;
   empresaLocalhost: string = environment.EMPRESA_LOCALHOST;
   urlMuup: string = environment.URL_API_MUUP;
-  empresaSubdominio$ = inject(Store).select(obtenerEmpresaSubdominio)
-  subdominioService = inject(SubdominioService)
+  subdominioService = inject(SubdominioService);
 
   constructor() {
-    this.empresaSubdominio$.subscribe((empresaSubdominio) => {
-      if(this.subdominioService.esSubdominioActual()){
-        this.urlSubDominio = this.urlSubDominio.replace('subdominio', empresaSubdominio);
-      } else {
-        this.urlSubDominio = this.urlSubDominio.replace('subdominio', this.empresaLocalhost);
-      }
-    });
+    if (this.subdominioService.esSubdominioActual()) {
+      this.urlSubDominio = this.urlSubDominio.replace(
+        'subdominio',
+        this.subdominioService.subdominioNombre()
+      );
+    } else {
+      this.urlSubDominio = this.urlSubDominio.replace(
+        'subdominio',
+        this.empresaLocalhost
+      );
+    }
   }
 }
