dc2e5066c6249c0b13ef537454073b20a135356b
andres <afcanop@gmail.com>
2024-04-03 16:14:29 -0500

    configuracion environment de pruebas

---
+++ src/environments/environment.pruebas.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  URL_API_MUUP: 'http://reddocapi.online',
+  URL_API_SUBDOMINIO: 'http://subdominio.reddocapi.online',
+  EMPRESA_LOCALHOST: 'demo',
+  dominioApp: '.reddoc.online',
+  appVersion: 'v8.1.8',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: true,
+  appThemeName: 'Metronic',
+  appPurchaseUrl: 'https://1.envato.market/EA4JP',
+  appHTMLIntegration:
+    'https://preview.keenthemes.com/metronic8/demo1/documentation/base/helpers/flex-layouts.html',
+  appPreviewUrl: 'https://preview.keenthemes.com/metronic8/angular/demo1/',
+  appPreviewAngularUrl:
+    'https://preview.keenthemes.com/metronic8/angular/demo1',
+  appPreviewDocsUrl: 'https://preview.keenthemes.com/metronic8/angular/docs',
+  appPreviewChangelogUrl:
+    'https://preview.keenthemes.com/metronic8/angular/docs/changelog',
+};
