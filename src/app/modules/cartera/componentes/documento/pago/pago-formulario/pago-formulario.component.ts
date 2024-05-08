f0d57896660b46d9c5a93d0d7a8b2d495f1ce3c4
andres <afcanop@gmail.com>
2024-05-08 14:02:51 -0500

    formulario de pago

--- src/app/modules/cartera/componentes/documento/pago/pago-formulario/pago-formulario.component.ts
+++ src/app/modules/cartera/componentes/documento/pago/pago-formulario/pago-formulario.component.ts
@@ -1,13 +1,54 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { General } from '@comun/clases/general';
+import { HttpService } from '@comun/services/http.service';
 
 @Component({
   selector: 'app-pago-formulario',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './pago-formulario.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PagoFormularioComponent { }
+export default class PagoFormularioComponent extends General implements OnInit {
+  formularioFactura: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpService: HttpService
+  ) {
+    super();
+  }
+
+  ngOnInit() {
+    this.consultarInformacion();
+    this.initForm();
+  }
+
+  initForm() {
+    const fechaActual = new Date(); // Obtener la fecha actual
+    const fechaVencimientoInicial = `${fechaActual.getFullYear()}-${(
+      fechaActual.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, '0')}-${fechaActual.getDate().toString().padStart(2, '0')}`;
+
+    this.formularioFactura = this.formBuilder.group({
+      empresa: [1],
+      contacto: ['', Validators.compose([Validators.required])],
+      contactoNombre: [''],
+      numero: [null],
+      fecha: [
+        fechaVencimientoInicial,
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(200),
+          Validators.pattern(/^[a-z-0-9.-_]*$/),
+        ]),
+      ],
+    });
+  }
+
+  consultarInformacion() {}
+}
