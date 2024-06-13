e49920f56202265a7525b1126fc181e700ec5d32
andres <afcanop@gmail.com>
2024-06-13 10:33:07 -0500

    cambios en facturacion

--- src/app/modules/profile/facturacion/facturacion.component.ts
+++ src/app/modules/profile/facturacion/facturacion.component.ts
@@ -1,23 +1,36 @@
-import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
+import { Contenedor } from './../../../interfaces/usuario/contenedor';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 import { General } from '@comun/clases/general';
 import { obtenerUsuarioId } from '@redux/selectors/usuario.selectors';
 import { Factura, ResumenService, Consumo } from '../services/resumen.service';
-import { zip } from 'rxjs';
+import { BehaviorSubject, Observable, of, zip } from 'rxjs';
 import { FechasService } from '@comun/services/fechas.service';
+import { ContenedorService } from '@modulos/contenedor/servicios/contenedor.service';
 
 @Component({
   selector: 'app-facturacion',
   templateUrl: './facturacion.component.html',
   styleUrls: ['./facturacion.component.scss'],
 })
-export class FacturacionComponent extends General implements OnInit, AfterViewInit {
+export class FacturacionComponent extends General implements OnInit {
   codigoUsuario = '';
   facturas: Factura[] = [];
   consumos: Consumo[] = [];
   consumoTotal = 0;
+  test = 1;
+  arrFacturasSeleccionados: any[] = [];
+  totalPagar = new BehaviorSubject(0); // Inicia con el valor 0
+
   constructor(
     private resumenService: ResumenService,
     public fechaServices: FechasService,
+    private contenedorServices: ContenedorService,
     private renderer: Renderer2
   ) {
     super();
@@ -30,51 +43,6 @@ export class FacturacionComponent extends General implements OnInit, AfterViewIn
     });
     this.consultarInformacion();
   }
-
-  ngAfterViewInit(): void {
-    this.insertScript();
-  }
-
-  public insertScript(): void {
-    const script = this.renderer.createElement('script');
-    this.renderer.setAttribute(
-      script,
-      'src',
-      'https://checkout.wompi.co/widget.js'
-    );
-    this.renderer.setAttribute(script, 'data-render', 'button');
-    this.renderer.setAttribute(
-      script,
-      'data-public-key',
-      'pub_test_HrxfoMdxFQFlRQ5be2n0jplrqpAViOKb'
-    );
-    this.renderer.setAttribute(script, 'data-currency', 'COP');
-    this.renderer.setAttribute(script, 'data-amount-in-cents', '100');
-    this.renderer.setAttribute(script, 'data-reference', '4XMPGKWWPKWQ');
-    this.renderer.setAttribute(
-      script,
-      'data-signature:integrity',
-      'test_integrity_ziIaff4k5fcDpZBdahGkKZXl1J08v7wd'
-    );
-    const form = document.getElementById('wompiWidget');
-    this.renderer.appendChild(form, script);
-  }
-
-  // insertScript(): void {
-  //   const script = document.createElement('script');
-  //   script.src = 'https://checkout.wompi.co/widget.js';
-  //   script.setAttribute('data-render', 'button');
-  //   script.setAttribute('data-public-key', 'pub_test_HrxfoMdxFQFlRQ5be2n0jplrqpAViOKb');
-  //   script.setAttribute('data-currency', 'COP');
-  //   script.setAttribute('data-amount-in-cents', '100');
-  //   script.setAttribute('data-reference', '4XMPGKWWPKWQ');
-  //   script.setAttribute('data-signature:integrity', 'test_integrity_ziIaff4k5fcDpZBdahGkKZXl1J08v7wd');
-
-  //   const buttons = document.querySelectorAll('.btn-wompiWidget');
-  //   buttons.forEach(button => {
-  //     button.appendChild(script.cloneNode(true));
-  //   });
-  // }
 
   consultarInformacion() {
     const hoy = new Date();
@@ -92,4 +60,86 @@ export class FacturacionComponent extends General implements OnInit, AfterViewIn
     });
   }
 
+  agregarRegistrosPagar(item: Factura) {
+    const index = this.arrFacturasSeleccionados.findIndex(
+      (documento) => documento.id === item.id
+    );
+    let valorActualPagar = this.totalPagar.getValue();
+
+    if (index !== -1) {
+      this.totalPagar.next(
+        valorActualPagar - parseInt(item.vr_saldo_enmascarado)
+      );
+      this.arrFacturasSeleccionados.splice(index, 1);
+      this.changeDetectorRef.detectChanges();
+    } else {
+      this.totalPagar.next(
+        valorActualPagar + parseInt(item.vr_saldo_enmascarado)
+      );
+      this.arrFacturasSeleccionados.push(item);
+      this.changeDetectorRef.detectChanges();
+    }
+
+    let referencia = '';
+    referencia = this.arrFacturasSeleccionados
+      .map((factura: Factura, index: number, array: Factura[]) => {
+        if (index === array.length - 1) {
+          return factura.id;
+        } else {
+          return factura.id + '-';
+        }
+      })
+      .join('');
+
+    if (referencia !== '') {
+      this.contenedorServices
+        .contenedorGenerarIntegridad({
+          referencia,
+          monto: `${this.totalPagar.getValue()}`,
+        })
+        .subscribe((respuesta) => {
+          this.habitarBtnWompi(respuesta);
+        });
+    }
+  }
+
+  habitarBtnWompi(respuesta: any) {
+    this.totalPagar.subscribe((total) => {
+      const wompiWidget = document.getElementById('wompiWidget');
+      if (total > 0) {
+        const script = this.renderer.createElement('script');
+        this.renderer.setAttribute(
+          script,
+          'src',
+          'https://checkout.wompi.co/widget.js'
+        );
+        this.renderer.setAttribute(script, 'data-render', 'button');
+        this.renderer.setAttribute(
+          script,
+          'data-public-key',
+          'pub_test_HrxfoMdxFQFlRQ5be2n0jplrqpAViOKb'
+        );
+        this.renderer.setAttribute(script, 'data-currency', 'COP');
+        this.renderer.setAttribute(
+          script,
+          'data-amount-in-cents',
+          total.toString()
+        );
+        this.renderer.setAttribute(script, 'data-reference', '4XMPGKWWPKWQ');
+        this.renderer.setAttribute(
+          script,
+          'data-signature:integrity',
+          respuesta.hash
+        );
+        while (wompiWidget?.firstChild) {
+          wompiWidget!.removeChild(wompiWidget!.firstChild);
+        }
+        this.renderer.appendChild(wompiWidget, script);
+      } else {
+        while (wompiWidget?.firstChild) {
+          wompiWidget!.removeChild(wompiWidget!.firstChild);
+        }
+      }
+    });
+  }
 }
--- src/app/modules/profile/services/resumen.service.ts
+++ src/app/modules/profile/services/resumen.service.ts
@@ -17,6 +17,7 @@ export interface Factura {
   fecha: string;
   id: number;
   tipo: string;
+  vr_saldo_enmascarado: string;
   vr_afectado: number;
   vr_saldo: number;
   vr_total: number;
