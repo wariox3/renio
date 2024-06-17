12f13d4d323bf062b7cffa44388653a5a1f23444
andres <afcanop@gmail.com>
2024-06-17 11:40:43 -0500

    test load 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,14 @@ import { locale as jpLang } from './modules/i18n/vocabs/jp';
 import { locale as deLang } from './modules/i18n/vocabs/de';
 import { locale as frLang } from './modules/i18n/vocabs/fr';
 import { ThemeModeService } from './_metronic/partials/layout/theme-mode-switcher/theme-mode.service';
+import {
+  Router,
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError,
+} from '@angular/router';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -18,12 +26,13 @@ import { ThemeModeService } from './_metronic/partials/layout/theme-mode-switche
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
+  public showOverlay = true;
 
   constructor(
     private translationService: TranslationService,
-    private modeService: ThemeModeService
+    private modeService: ThemeModeService,
+    private router: Router
   ) {
-
     // register translations
     this.translationService.loadTranslations(
       enLang,
@@ -33,9 +42,32 @@ export class AppComponent implements OnInit {
       deLang,
       frLang
     );
+
+    router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event);
+    });
   }
 
   ngOnInit() {
     this.modeService.init();
   }
+
+  // Shows and hides the loading spinner during RouterEvent changes
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.showOverlay = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.showOverlay = false;
+    }
+
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.showOverlay = false;
+    }
+    if (event instanceof NavigationError) {
+      this.showOverlay = false;
+    }
+  }
+
 }
