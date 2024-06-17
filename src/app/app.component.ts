d60eee47ed81d216489a1e312cab39fdbd9a3927
andres <afcanop@gmail.com>
2024-06-17 11:57:13 -0500

    test load 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,6 @@ import { locale as jpLang } from './modules/i18n/vocabs/jp';
 import { locale as deLang } from './modules/i18n/vocabs/de';
 import { locale as frLang } from './modules/i18n/vocabs/fr';
 import { ThemeModeService } from './_metronic/partials/layout/theme-mode-switcher/theme-mode.service';
-import {
-  Router,
-  Event as RouterEvent,
-  NavigationStart,
-  NavigationEnd,
-  NavigationCancel,
-  NavigationError,
-} from '@angular/router';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -26,13 +18,13 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
-  public showOverlay = true;
+  public showOverlay = false;
 
   constructor(
     private translationService: TranslationService,
-    private modeService: ThemeModeService,
-    private router: Router
+    private modeService: ThemeModeService
   ) {
+
     // register translations
     this.translationService.loadTranslations(
       enLang,
@@ -43,31 +35,17 @@ export class AppComponent implements OnInit {
       frLang
     );
 
-    router.events.subscribe((event: RouterEvent) => {
-      this.navigationInterceptor(event);
-    });
+    // Detect when the window is fully loaded
+    window.addEventListener('load', this.onWindowLoad.bind(this));
   }
 
   ngOnInit() {
     this.modeService.init();
   }
 
-  // Shows and hides the loading spinner during RouterEvent changes
-  navigationInterceptor(event: RouterEvent): void {
-    if (event instanceof NavigationStart) {
-      this.showOverlay = true;
-    }
-    if (event instanceof NavigationEnd) {
+  onWindowLoad() {
+    setTimeout(() => {
       this.showOverlay = false;
-    }
-
-    // Set loading state to false in both of the below events to hide the spinner in case a request fails
-    if (event instanceof NavigationCancel) {
-      this.showOverlay = false;
-    }
-    if (event instanceof NavigationError) {
-      this.showOverlay = false;
-    }
+    }, 500);
   }
-
 }
