132ad1d29fb276b0f5f66392c71e0158f5ccb4d3
afcanop <afcanop@gmail.com>
2024-06-27 10:51:17 -0500

    mensaje de complejar

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,17 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { General } from '@comun/clases/general';
 import { HttpService } from '@comun/services/http.service';
 import { empresaActionInit } from '@redux/actions/empresa.actions';
+import { CardComponent } from "../../comun/componentes/card/card.component";
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
     standalone: true,
+    imports: [CardComponent]
 })
 export class DashboardComponent extends General implements OnInit {
   constructor(private httpService: HttpService) {
     super();
   }
+
+  rededoc_id: string | null;
 
   ngOnInit() {
     this.consultarInformacion();
@@ -22,6 +26,7 @@ export class DashboardComponent extends General implements OnInit {
     this.httpService
       .getDetalle<any>(`general/empresa/1/`)
       .subscribe((empresa: any) => {
+        this.rededoc_id = empresa.rededoc_id
         this.store.dispatch(empresaActionInit({ empresa }));
         this.changeDetectorRef.detectChanges();
       });
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { ModalsModule, WidgetsModule } from '../../_metronic/partials';
 import {  NgbOffcanvasModule } from '@ng-bootstrap/ng-bootstrap';
+import { General } from '@comun/clases/general';
+import { obtenerEmpresRededoc_id } from '@redux/selectors/empresa.selectors';
 
 @NgModule({
     imports: [
