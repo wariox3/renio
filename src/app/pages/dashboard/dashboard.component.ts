7dff5210a02d26dfc2a1f1a59f8fb9803698b766
afcanop <afcanop@gmail.com>
2024-06-29 10:46:36 -0500

    se agrega btn de omitir

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { General } from '@comun/clases/general';
 import { HttpService } from '@comun/services/http.service';
-import { empresaActionInit } from '@redux/actions/empresa.actions';
-import { CardComponent } from "../../comun/componentes/card/card.component";
+import {
+  empresaActionInit,
+  empresaActualizacionAsisteneElectronico,
+} from '@redux/actions/empresa.actions';
+import { CardComponent } from '../../comun/componentes/card/card.component';
 import { RouterModule } from '@angular/router';
 import { Empresa } from '@interfaces/contenedor/empresa';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { EmpresaService } from '@modulos/empresa/servicios/empresa.service';
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss'],
-    standalone: true,
-    imports: [CardComponent, RouterModule]
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  standalone: true,
+  imports: [CardComponent, RouterModule, NgbTooltipModule],
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+  host: { class: 'd-block' },
 })
 export class DashboardComponent extends General implements OnInit {
-  constructor(private httpService: HttpService) {
+  constructor(
+    private httpService: HttpService,
+    private empresaService: EmpresaService
+  ) {
     super();
   }
 
@@ -28,9 +38,20 @@ export class DashboardComponent extends General implements OnInit {
     this.httpService
       .getDetalle<Empresa>(`general/empresa/1/`)
       .subscribe((empresa) => {
-        this.asistente_electronico = empresa.asistente_electronico
+        this.asistente_electronico = empresa.asistente_electronico;
         this.store.dispatch(empresaActionInit({ empresa }));
         this.changeDetectorRef.detectChanges();
       });
+  }
+
+  finalizarProceso() {
+    this.empresaService.finalizarProceso().subscribe((respuesta) => {
+      this.store.dispatch(
+        empresaActualizacionAsisteneElectronico({
+          asistente_electronico: respuesta.asistente_termiando,
+        })
+      );
+      this.consultarInformacion()
+    });
   }
 }
