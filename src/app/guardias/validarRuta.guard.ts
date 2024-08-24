8dbb2c1506732241c65af6b0fb514c3af0137f29
afcanop <afcanop@gmail.com>
2024-08-24 10:56:23 -0500

    feat: validacion en ruta

--- src/app/guardias/validarRuta.guard.ts
+++ src/app/guardias/validarRuta.guard.ts
@@ -1,7 +1,11 @@
 import { inject } from '@angular/core';
 import { Router, type CanActivateChildFn } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { obtenerUsuarioFechaLimitePago, obtenerUsuarioVrSaldo } from '@redux/selectors/usuario.selectors';
+import { obtenerContenedorSeleccion } from '@redux/selectors/contenedor.selectors';
+import {
+  obtenerUsuarioFechaLimitePago,
+  obtenerUsuarioVrSaldo,
+} from '@redux/selectors/usuario.selectors';
 import { zip } from 'rxjs';
 
 export const validarRutaGuard: CanActivateChildFn = (childRoute, state) => {
@@ -14,22 +18,21 @@ export const validarRutaGuard: CanActivateChildFn = (childRoute, state) => {
   zip([
     store.select(obtenerUsuarioFechaLimitePago),
     store.select(obtenerUsuarioVrSaldo),
+    store.select(obtenerContenedorSeleccion),
   ]).subscribe((respuesta) => {
     usuarioFechaLimitePago = new Date(respuesta[0]);
-    usuarioFechaLimitePago.setDate(
-      usuarioFechaLimitePago.getDate() + 1
-    );
+    usuarioFechaLimitePago.setDate(usuarioFechaLimitePago.getDate() + 1);
     usuarioSaldo = respuesta[1];
-    if (
-      usuarioSaldo > 0 &&
-      usuarioFechaLimitePago < fechaActual
-    ) {
+    if (usuarioSaldo > 0 && usuarioFechaLimitePago < fechaActual) {
       persimsos = false;
+    } else {
+      if (respuesta[2] === false) {
+        persimsos = false;
+      }
     }
   });
 
-
-  if(!persimsos){
+  if (!persimsos) {
     router.navigate(['/contenedor/lista']);
   }
 
