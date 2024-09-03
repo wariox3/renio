2f36695d798b6d0e8fb8423ffcab10f0a6b681d0
afcanop <afcanop@gmail.com>
2024-09-03 14:05:31 -0500

    feat: cambios en ui filtros

--- src/app/comun/componentes/base-filtro/base-filtro.component.ts
+++ src/app/comun/componentes/base-filtro/base-filtro.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
@@ -42,6 +42,8 @@ import {
     KeysPipe,
     NgbTooltipModule,
   ],
+  styleUrls: ['./base-filtro.component.scss'],
+  encapsulation: ViewEncapsulation.None,
   animations: [
     trigger('fadeInOut', [
       state(
