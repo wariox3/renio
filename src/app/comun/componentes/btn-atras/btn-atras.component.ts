745b542040fca260b5e6891f4a4acd7a98651c69
afcanop <afcanop@gmail.com>
2024-10-23 15:27:52 -0500

    feracto: ui de los detalles de venta

--- src/app/comun/componentes/btn-atras/btn-atras.component.ts
+++ src/app/comun/componentes/btn-atras/btn-atras.component.ts
@@ -16,6 +16,7 @@ import { TranslateModule } from '@ngx-translate/core';
         (click)="navegarAtras()"
         class="btn btn-sm btn-flex bg-body btn-color-gray-700 btn-active-primary"
       >
+      
         <i class="bi bi-arrow-left fs-6"
           ><span class="path1"></span><span class="path2"></span
         ></i>
