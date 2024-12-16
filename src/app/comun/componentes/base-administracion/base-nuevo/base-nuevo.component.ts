354969106965d8b6a010d66ddec987724550632d
afcanop <afcanop@gmail.com>
2024-12-16 14:54:25 -0500

    fix: remover de changeDetectorRef de base nuevo administradores

--- src/app/comun/componentes/base-administracion/base-nuevo/base-nuevo.component.ts
+++ src/app/comun/componentes/base-administracion/base-nuevo/base-nuevo.component.ts
@@ -19,11 +19,10 @@ import { HttpService } from '@comun/services/http.service';
   templateUrl: './base-nuevo.component.html',
 })
 export class BaseNuevoComponent extends General implements AfterViewInit {
-  generarPDF = false;
   @ViewChild('dynamicComponentContainer', { read: ViewContainerRef })
   componenteDinamico: ViewContainerRef;
 
-  constructor(private httpService: HttpService) {
+  constructor() {
     super();
   }
 
@@ -33,40 +32,14 @@ export class BaseNuevoComponent extends General implements AfterViewInit {
 
   async loadComponente() {
     this.componenteDinamico.clear();
-    this.activatedRoute.queryParams.subscribe((parametros) => {
-        this.modelo = parametros.itemNombre;
-       if (parametros.submodelo) {
-        this.modelo = parametros.submodelo;
-        this.changeDetectorRef.detectChanges()
-       }
-    });
+    this.modelo = this.parametrosUrl?.itemNombre!;
+    if (this.parametrosUrl?.submodelo) {
+      this.modelo = this.parametrosUrl?.submodelo;
+    }
     let posicion: keyof typeof Componetes = this.modelo;
     let componete = await (await Componetes[posicion].formulario()).default;
     let componeteCargado = this.componenteDinamico.createComponent(componete);
     componeteCargado.changeDetectorRef.detectChanges();
   }
 
-  emitir() {
-    this.httpService
-      .post('general/documento/emitir/', { id: this.detalle })
-      .subscribe((respuesta: any) => {
-        this.alertaService.mensajaExitoso('Documento aprobado');
-        window.location.reload();
-      });
-  }
-
-  imprimir() {
-    this.generarPDF = true;
-    this.httpService.descargarArchivo('general/documento/imprimir/', {
-      filtros: [],
-      limite: 50,
-      desplazar: 0,
-      ordenamientos: [],
-      limite_conteo: 10000,
-      modelo: '',
-      tipo: '',
-      documento_tipo_id: 1,
-      documento_id: this.detalle,
-    });
-  }
 }
