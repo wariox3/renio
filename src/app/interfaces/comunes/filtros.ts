fc1f8a0d4d6e771b89ec4689fd1bf17d512339b4
sebastian <sebastian.h.piedrahita@gmail.com>
2024-12-17 15:20:05 -0500

    feat: filtros tipados

--- src/app/interfaces/comunes/filtros.ts
+++ src/app/interfaces/comunes/filtros.ts
@@ -5,10 +5,52 @@ export interface Listafiltros {
   tipo: 'Texto' | 'Numero' | 'Booleano' | 'Fecha';
 }
 
-export interface FiltrosAplicados {
+export interface FiltrosAplicados extends Filtros {  
+  visualizarBtnAgregarFiltro?: boolean;
+}
+
+export interface Filtros {
   propiedad: string;
   operador: string;
   valor1: string;
   valor2: string;
-  visualizarBtnAgregarFiltro: boolean;
 }
+
+export interface ParametrosFiltros {
+  limite: number;
+  desplazar: number;
+  ordenamientos: string[];
+  limite_conteo: number;
+  modelo: Modelo;
+  serializador: Serializador;
+  filtros: FiltrosAplicados[];
+}
+
+type Serializador =
+  | 'ListaBuscar'
+  | 'ListaAutocompletar'
+  | 'Informe'
+  | 'Referencia'
+  | 'EventoCompra'
+  | 'Excel'
+  | 'NominaExcel'
+  | 'Nomina'
+  | 'Adicionar';
+
+type Modelo =
+  | 'GenDocumento'
+  | 'GenCiudad'
+  | 'GenIdentificacion'
+  | 'GenRegimen'
+  | 'GenTipoPersona'
+  | 'GenPrecio'
+  | 'GenMetodoPago'
+  | 'GenAsesor'
+  | 'GenPlazoPago'
+  | 'GenBanco'
+  | 'GenCuentaBancoClase'
+  | 'HumPeriodo'
+  | 'HumConcepto'
+  | 'HumAdicional'
+  | 'HumProgramacionDetalle'
+  | 'HumContrato';
