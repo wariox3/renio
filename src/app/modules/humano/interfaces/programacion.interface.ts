76de342e68c218b8b91d640f0661cb4274a7ba23
sebastian <sebastian.h.piedrahita@gmail.com>
2024-12-27 11:47:35 -0500

    refactor: prgramacion service interfaces

---
+++ src/app/modules/humano/interfaces/programacion.interface.ts
@@ -0,0 +1,47 @@
+export interface GenerarProgramacionRespuesta {
+  total: number;
+  devengado: number;
+  deduccion: number;
+}
+
+export interface DesgenerarProgramacionRespuesta {
+  mensaje: string;
+}
+
+export interface CargarContratosRespuesta {
+  contratos: number;
+}
+
+export interface ProgramacionRespuesta {
+  id: number;
+  fecha_desde: string;
+  fecha_hasta: string;
+  fecha_hasta_periodo: string;
+  nombre: string;
+  devengado: number;
+  deduccion: number;
+  total: number;
+  contratos: number;
+  dias: number;
+  comentario: any;
+  pago_horas: boolean;
+  pago_auxilio_transporte: boolean;
+  pago_incapacidad: boolean;
+  pago_licencia: boolean;
+  pago_vacacion: boolean;
+  descuento_salud: boolean;
+  descuento_pension: boolean;
+  descuento_fondo_solidaridad: boolean;
+  descuento_retencion_fuente: boolean;
+  adicional: boolean;
+  descuento_credito: boolean;
+  descuento_embargo: boolean;
+  pago_tipo_id: number;
+  pago_tipo_nombre: string;
+  grupo_id: number;
+  grupo_nombre: string;
+  periodo_id: number;
+  periodo_nombre: string;
+  estado_generado: boolean;
+  estado_aprobado: boolean;
+}
--- src/app/modules/humano/servicios/programacion.service.ts
+++ src/app/modules/humano/servicios/programacion.service.ts
@@ -1,39 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Subdominio } from '@comun/clases/subdomino';
 import { HttpService } from '@comun/services/http.service';
+import {
+  CargarContratosRespuesta,
+  DesgenerarProgramacionRespuesta,
+  GenerarProgramacionRespuesta,
+  ProgramacionRespuesta,
+} from '../interfaces/programacion.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProgramacionService extends Subdominio {
-
   constructor(private httpService: HttpService) {
     super();
   }
 
   guardarProgramacion(data: any) {
-    return this.httpService.post<any[]>(`humano/programacion/`, data);
+    return this.httpService.post<ProgramacionRespuesta>(`humano/programacion/`, data);
   }
 
   consultarDetalle(id: number) {
-    return this.httpService.getDetalle<any>(`humano/programacion/${id}/`);
+    return this.httpService.getDetalle<ProgramacionRespuesta>(`humano/programacion/${id}/`);
   }
 
   actualizarDatosProgramacion(id: number, data: any) {
-    return this.httpService.put<any>(`humano/programacion/${id}/`, data);
+    return this.httpService.put<ProgramacionRespuesta>(`humano/programacion/${id}/`, data);
   }
 
   cargarContratos(data: any) {
-    return this.httpService.post<any>(`humano/programacion/cargar-contrato/`, data);
+    return this.httpService.post<CargarContratosRespuesta>(
+      `humano/programacion/cargar-contrato/`,
+      data
+    );
   }
 
-  generar(data: any) {
-    return this.httpService.post<any>(`humano/programacion/generar/`, data);
+  generar(data: { id: number }) {
+    return this.httpService.post<GenerarProgramacionRespuesta>(
+      `humano/programacion/generar/`,
+      data
+    );
   }
 
-  desgenerar(data: any) {
-    return this.httpService.post<any>(`humano/programacion/desgenerar/`, data);
+  desgenerar(data: { id: number }) {
+    return this.httpService.post<DesgenerarProgramacionRespuesta>(
+      `humano/programacion/desgenerar/`,
+      data
+    );
   }
-  
-
 }
