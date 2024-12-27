e09d7d0cacc945ee6fdceab5b6c631e9fb4ad499
sebastian <sebastian.h.piedrahita@gmail.com>
2024-12-27 13:22:57 -0500

    refactor: tipar pago interface

--- src/app/modules/venta/servicios/factura.service.ts
+++ src/app/modules/venta/servicios/factura.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subdominio } from '@comun/clases/subdomino';
 import { HttpService } from '@comun/services/http.service';
+import { DocumentoFacturaRespuesta } from '@interfaces/comunes/factura/factura.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,23 +10,32 @@ export class FacturaService {
   constructor(private httpService: HttpService) {}
 
   guardarFactura(data: any) {
-    return this.httpService.post<any>('general/documento/', data);
+    return this.httpService.post<{ documento: DocumentoFacturaRespuesta }>(
+      'general/documento/',
+      data
+    );
   }
 
   actualizarDatosFactura(id: number, data: any) {
-    return this.httpService.put<any>(`general/documento/${id}/`, data);
+    return this.httpService.put<{ documento: DocumentoFacturaRespuesta }>(
+      `general/documento/${id}/`,
+      data
+    );
   }
 
   consultarDetalle(id: number) {
-    return this.httpService.get<any>(`general/documento/${id}/`);
+    return this.httpService.get<{ documento: DocumentoFacturaRespuesta }>(
+      `general/documento/${id}/`
+    );
   }
 
-  aprobar(data: number){
+  aprobar(data: number) {
     return this.httpService.post<any>('general/documento/aprobar/', data);
   }
 
   emitir(id: number) {
-    return this.httpService
-      .post('general/documento/emitir/', { documento_id: id })
+    return this.httpService.post('general/documento/emitir/', {
+      documento_id: id,
+    });
   }
 }
