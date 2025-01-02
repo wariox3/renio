835ebbf348c80d391de0eeafb2d097bf87203d7a
afcanop <afcanop@gmail.com>
2025-01-02 09:15:59 -0500

    refactror: se retira codigo no necesario en app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,16 @@
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  OnInit,
+  Component
 } from '@angular/core';
 import { TranslationService } from './modules/i18n';
 // language list
-import { locale as enLang } from './modules/i18n/vocabs/en';
-import { locale as chLang } from './modules/i18n/vocabs/ch';
-import { locale as esLang } from './modules/i18n/vocabs/es';
-import { locale as jpLang } from './modules/i18n/vocabs/jp';
-import { locale as deLang } from './modules/i18n/vocabs/de';
-import { locale as frLang } from './modules/i18n/vocabs/fr';
-import { ThemeModeService } from './_metronic/partials/layout/theme-mode-switcher/theme-mode.service';
 import { RouterOutlet } from '@angular/router';
+import { locale as chLang } from './modules/i18n/vocabs/ch';
+import { locale as deLang } from './modules/i18n/vocabs/de';
+import { locale as enLang } from './modules/i18n/vocabs/en';
+import { locale as esLang } from './modules/i18n/vocabs/es';
+import { locale as frLang } from './modules/i18n/vocabs/fr';
+import { locale as jpLang } from './modules/i18n/vocabs/jp';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -25,13 +22,10 @@ import { RouterOutlet } from '@angular/router';
     standalone: true,
     imports: [RouterOutlet],
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent  {
 
   constructor(
     private translationService: TranslationService,
-    private modeService: ThemeModeService,
-    private changeDetectorRef: ChangeDetectorRef
   ) {
     // register translations
     this.translationService.loadTranslations(
@@ -44,8 +38,5 @@ export class AppComponent implements OnInit {
     );
   }
 
-  ngOnInit() {
-    this.modeService.init();
-  }
 
 }
