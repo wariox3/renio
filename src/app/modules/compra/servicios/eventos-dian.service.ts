37459a4f652ff3ffccd01a67c34de7476cf67272
sebastian <sebastian.h.piedrahita@gmail.com>
2025-01-02 15:30:31 -0500

    fix: correrccion api

--- src/app/modules/compra/servicios/eventos-dian.service.ts
+++ src/app/modules/compra/servicios/eventos-dian.service.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpService } from '@comun/services/http.service';
 
@@ -6,7 +5,7 @@ import { HttpService } from '@comun/services/http.service';
   providedIn: 'root',
 })
 export class EventosDianService {
-  constructor(private httpService: HttpClient) {}
+  constructor(private httpService: HttpService) {}
 
   emitirEvento(data: any) {
     return this.httpService.post<any>('general/documento/emitir-evento/', data);
