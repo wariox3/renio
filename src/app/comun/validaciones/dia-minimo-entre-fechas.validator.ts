0ca752a461dacda4a60886edad6b04fbbf96582f
sebastian <sebastian.h.piedrahita@gmail.com>
2025-01-07 15:46:12 -0500

    fix: validar mes febrero

--- src/app/comun/validaciones/dia-minimo-entre-fechas.validator.ts
+++ src/app/comun/validaciones/dia-minimo-entre-fechas.validator.ts
@@ -5,8 +5,12 @@ export function minimumDaysBetweenDates(days: number): ValidatorFn {
     const fechaDesde = control.get('fecha_desde')?.value;
     const fechaHasta = control.get('fecha_hasta')?.value;
 
+    if (esFebrero(fechaDesde)) {
+      days = casoEspecialFebrero(fechaDesde, days);
+    }
+
     if (!fechaDesde || !fechaHasta) {
-      return null;  // Si alguna fecha está vacía, no hacemos la validación
+      return null; // Si alguna fecha está vacía, no hacemos la validación
     }
 
     // Convertimos los valores a fechas nativas de JavaScript
@@ -18,6 +22,31 @@ export function minimumDaysBetweenDates(days: number): ValidatorFn {
     const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
 
     // Si la diferencia en días es menor al mínimo requerido, retorna un error
-    return diffInDays === days-1 ? null : { minimoDias: true };
+    return diffInDays === days - 1 ? null : { minimoDias: true };
   };
+}
+
+function casoEspecialFebrero(fecha: string, days: number): number {
+  const [yearDesde, monthDesde, dayDesde] = fecha.split('-').map(Number);
+  const ultimoDia = new Date(yearDesde, monthDesde, 0);
+  const diferenciaDias = ultimoDia.getDate() - dayDesde;
+
+  // si el periodo es mayor a el ultimo dia del mes, entonces devuelve el ultimo dia del mes
+  if (days > ultimoDia.getDate()) {
+    return ultimoDia.getDate();
+  }
+
+  // ejemplo: validacion del 1 al 15
+  if (diferenciaDias > days) {
+    return days;
+  } else {
+    return diferenciaDias + 1;
+  }
+}
+
+function esFebrero(fecha: string): boolean {
+  const [yearDesde, monthDesde, dayDesde] = fecha.split('-').map(Number);
+  const fechaDesdeObj = new Date(yearDesde, monthDesde - 1, dayDesde);
+
+  return fechaDesdeObj.getMonth() === 1; // getMonth() devuelve 1 para febrero (meses van de 0 a 11)
 }
