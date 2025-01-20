8aa41e5efd47650ed8c651bdaa9dab47c2a07ca8
sebastian <sebastian.h.piedrahita@gmail.com>
2025-01-20 17:01:58 -0500

    feat(cuenta-formulario): cambios en la UI del formulario (incompleto)

--- src/app/modules/contabilidad/paginas/administrador/cuenta/cuenta-formulario/cuenta-formulario.component.ts
+++ src/app/modules/contabilidad/paginas/administrador/cuenta/cuenta-formulario/cuenta-formulario.component.ts
@@ -28,6 +28,12 @@ import { forkJoin, map, of, switchMap, tap } from 'rxjs';
 import { TituloAccionComponent } from '../../../../../../comun/componentes/titulo-accion/titulo-accion.component';
 import { validarNoIniciaCon } from '@comun/validaciones/validar-primer-caracter.validator';
 import { SoloNumerosDirective } from '@comun/directive/solo-numeros.directive';
+import { NgSelectModule } from '@ng-select/ng-select';
+import {
+  RegistroAutocompletarConCuentaClase,
+  RegistroAutocompletarConCuentaCuenta,
+  RegistroAutocompletarConCuentaGrupo,
+} from '@interfaces/comunes/autocompletar/contabilidad/con-cuenta.interface';
 
 @Component({
   selector: 'app-cuenta-formulario',
@@ -36,6 +42,7 @@ import { SoloNumerosDirective } from '@comun/directive/solo-numeros.directive';
   imports: [
     CommonModule,
     FormsModule,
+    NgSelectModule,
     ReactiveFormsModule,
     TranslateModule,
     CardComponent,
@@ -54,6 +61,10 @@ export default class ItemFormularioComponent extends General implements OnInit {
   inputImpuestos: HTMLInputElement;
   arrCuentaClase: any = [];
 
+  public cuentaClaseLista: RegistroAutocompletarConCuentaClase[] = [];
+  public cuentaGrupoLista: RegistroAutocompletarConCuentaGrupo[] = [];
+  public cuentaCuentaLista: RegistroAutocompletarConCuentaCuenta[] = [];
+
   private readonly _generalService = inject(GeneralService);
 
   constructor(
@@ -64,62 +75,64 @@ export default class ItemFormularioComponent extends General implements OnInit {
   }
 
   ngOnInit() {
-    this.iniciarFormulario();
+    this._iniciarFormulario();
+    this._consultarInformacionInicial();
+    this._listenFormularioCuentaClaseCampo();
 
     if (this.detalle && this.ocultarBtnAtras === false) {
       this.consultardetalle();
     }
 
-    this.formularioConCuenta
-      .get('codigo')
-      ?.valueChanges.subscribe((value: string) => {
-        if (value && value.length > 0) {
-          if (value && !isNaN(Number(value.charAt(0)))) {
-            this.formularioConCuenta
-            .get('cuenta_clase')
-            ?.setValue(value.charAt(0));
-            this.formularioConCuenta.get('cuenta_clase')?.markAsTouched();
-          }
-          if (value.length >= 2) {
-            this.formularioConCuenta
-              .get('cuenta_grupo')
-              ?.setValue(value.substring(0, 2));
-            this.validarGrupo(value.substring(0, 2));
-            this.formularioConCuenta.get('cuenta_grupo')?.markAsTouched();
-          } else {
-            this.formularioConCuenta.get('cuenta_grupo')?.setValue('');
-          }
-          if (value.length >= 4) {
-            this.formularioConCuenta
-              .get('cuenta_subcuenta')
-              ?.setValue(value.substring(0, 4));
-            this.formularioConCuenta.get('cuenta_subcuenta')?.markAsTouched();
-          } else {
-            this.formularioConCuenta.get('cuenta_subcuenta')?.setValue('');
-          }
+    // this.formularioConCuenta
+    //   .get('codigo')
+    //   ?.valueChanges.subscribe((value: string) => {
+    //     if (value && value.length > 0) {
+    //       if (value && !isNaN(Number(value.charAt(0)))) {
+    //         this.formularioConCuenta
+    //           .get('cuenta_clase')
+    //           ?.setValue(value.charAt(0));
+    //         this.formularioConCuenta.get('cuenta_clase')?.markAsTouched();
+    //       }
+    //       if (value.length >= 2) {
+    //         this.formularioConCuenta
+    //           .get('cuenta_grupo')
+    //           ?.setValue(value.substring(0, 2));
+    //         this.validarGrupo(value.substring(0, 2));
+    //         this.formularioConCuenta.get('cuenta_grupo')?.markAsTouched();
+    //       } else {
+    //         this.formularioConCuenta.get('cuenta_grupo')?.setValue('');
+    //       }
+    //       if (value.length >= 4) {
+    //         this.formularioConCuenta
+    //           .get('cuenta_subcuenta')
+    //           ?.setValue(value.substring(0, 4));
+    //         this.formularioConCuenta.get('cuenta_subcuenta')?.markAsTouched();
+    //       } else {
+    //         this.formularioConCuenta.get('cuenta_subcuenta')?.setValue('');
+    //       }
 
-          let nivel = 1;
-          if (value.length > 1 && value.length <= 2) {
-            nivel = 2;
-          } else if (value.length > 2 && value.length <= 4) {
-            nivel = 3;
-          } else if (value.length > 4 && value.length <= 6) {
-            nivel = 4;
-          } else if (value.length > 6 && value.length <= 8) {
-            nivel = 5;
-          }
+    //       let nivel = 1;
+    //       if (value.length > 1 && value.length <= 2) {
+    //         nivel = 2;
+    //       } else if (value.length > 2 && value.length <= 4) {
+    //         nivel = 3;
+    //       } else if (value.length > 4 && value.length <= 6) {
+    //         nivel = 4;
+    //       } else if (value.length > 6 && value.length <= 8) {
+    //         nivel = 5;
+    //       }
 
-          this.formularioConCuenta.get('nivel')?.setValue(nivel);
-        } else {
-          this.formularioConCuenta.get('cuenta_clase')?.setValue('');
-          this.formularioConCuenta.get('cuenta_grupo')?.setValue('');
-          this.formularioConCuenta.get('cuenta_subcuenta')?.setValue('');
-          this.formularioConCuenta.get('nivel')?.setValue(null);
-        }
-      });
+    //       this.formularioConCuenta.get('nivel')?.setValue(nivel);
+    //     } else {
+    //       this.formularioConCuenta.get('cuenta_clase')?.setValue('');
+    //       this.formularioConCuenta.get('cuenta_grupo')?.setValue('');
+    //       this.formularioConCuenta.get('cuenta_subcuenta')?.setValue('');
+    //       this.formularioConCuenta.get('nivel')?.setValue(null);
+    //     }
+    //   });
   }
 
-  iniciarFormulario() {
+  private _iniciarFormulario() {
     this.formularioConCuenta = this.formBuilder.group({
       codigo: [
         null,
@@ -134,13 +147,9 @@ export default class ItemFormularioComponent extends General implements OnInit {
         '',
         Validators.compose([Validators.required, Validators.maxLength(100)]),
       ],
-      cuenta_clase: [null, Validators.compose([validarNoIniciaCon('0')])],
-      cuenta_grupo: [null, Validators.compose([validarNoIniciaCon('0')])],
-      cuenta_subcuenta: [
-        null,
-        Validators.compose([validarNoIniciaCon('0')]),
-      ],
-      nivel: [null],
+      cuenta_clase: [null, [Validators.required]],
+      cuenta_grupo: [null, [Validators.required]],
+      cuenta_cuenta: [null, [Validators.required]],
       exige_base: [false],
       exige_tercero: [false],
       exige_grupo: [false],
@@ -156,101 +165,111 @@ export default class ItemFormularioComponent extends General implements OnInit {
     // Verificar si el formulario es válido
     if (this.formularioConCuenta.valid) {
       // Validar que los campos clase, grupo y subcuenta no estén vacíos
-      const cuentaClase = this.formularioConCuenta.get('cuenta_clase')?.value;
-      const cuentaGrupo = this.formularioConCuenta.get('cuenta_grupo')?.value;
-      const cuentaSubcuenta =
-        this.formularioConCuenta.get('cuenta_subcuenta')?.value;
+      // const cuentaClase = this.formularioConCuenta.get('cuenta_clase')?.value;
+      // const cuentaGrupo = this.formularioConCuenta.get('cuenta_grupo')?.value;
+      // const cuentaSubcuenta =
+      //   this.formularioConCuenta.get('cuenta_subcuenta')?.value;
 
-      if (!cuentaClase || !cuentaGrupo || !cuentaSubcuenta) {
-        this.alertaService.mensajeError(
-          'Error',
-          'Los campos de clase, grupo y subcuenta son obligatorios.'
-        );
-        return;
+      // if (!cuentaClase || !cuentaGrupo || !cuentaSubcuenta) {
+      //   this.alertaService.mensajeError(
+      //     'Error',
+      //     'Los campos de clase, grupo y subcuenta son obligatorios.'
+      //   );
+      //   return;
+      // }
+
+      if (this.detalle) {
+        this.cuentaService
+          .actualizarDatos(this.detalle, this.formularioConCuenta.value)
+          .subscribe();
+      } else {
+        this.cuentaService
+          .guardarCuenta(this.formularioConCuenta.value)
+          .subscribe();
       }
 
-      forkJoin([
-        this.validarCuentaClase(
-          this.formularioConCuenta.get('codigo')?.value.charAt(0)
-        ),
-        this.validarGrupo(
-          this.formularioConCuenta.get('codigo')?.value.substring(0, 2)
-        ),
-        this.validarSubCuenta(
-          this.formularioConCuenta.get('codigo')?.value.substring(0, 4)
-        ),
-      ])
-        .pipe(
-          map((respuesta) => {
-            let errores = false;
-            if (respuesta[0].registros.length > 0) {
-              this.formularioConCuenta.get('cuenta_clase')?.setErrors(null);
-            } else {
-              errores = true;
-              this.formularioConCuenta
-                .get('cuenta_clase')
-                ?.setErrors({ grupoNoValido: true });
-              this.changeDetectorRef.detectChanges();
-            }
-            if (respuesta[1].registros.length > 0) {
-              this.formularioConCuenta.get('cuenta_grupo')?.setErrors(null);
-            } else {
-              errores = true;
-              this.formularioConCuenta
-                .get('cuenta_grupo')
-                ?.setErrors({ grupoNoValido: true });
-              this.changeDetectorRef.detectChanges();
-            }
-            if (respuesta[2].registros.length > 0) {
-              this.formularioConCuenta.get('cuenta_subcuenta')?.setErrors(null);
-            } else {
-              errores = true;
-              this.formularioConCuenta
-                .get('cuenta_subcuenta')
-                ?.setErrors({ grupoNoValido: true });
-              this.changeDetectorRef.detectChanges();
-            }
-            return errores;
-          }),
-          switchMap((respuestaErrores) => {
-            if (respuestaErrores === false) {
-              if (this.detalle) {
-                return this.cuentaService.actualizarDatos(
-                  this.detalle,
-                  this.formularioConCuenta.value
-                );
-              } else {
-                return this.cuentaService.guardarCuenta(
-                  this.formularioConCuenta.value
-                );
-              }
-            } else {
-              return of(null);
-            }
-          }),
-          tap((respuesta) => {
-            if (respuesta) {
-              if (this.detalle) {
-                this.alertaService.mensajaExitoso(
-                  'Se actualizó la información'
-                );
-              } else {
-                this.alertaService.mensajaExitoso(
-                  'Se actualizó la información'
-                );
-              }
-              this.activatedRoute.queryParams.subscribe((parametro) => {
-                this.router.navigate([`/administrador/detalle`], {
-                  queryParams: {
-                    ...parametro,
-                    detalle: respuesta?.id,
-                  },
-                });
-              });
-            }
-          })
-        )
-        .subscribe();
+      // forkJoin([
+      //   this.validarCuentaClase(
+      //     this.formularioConCuenta.get('codigo')?.value.charAt(0)
+      //   ),
+      //   this.validarGrupo(
+      //     this.formularioConCuenta.get('codigo')?.value.substring(0, 2)
+      //   ),
+      //   this.validarSubCuenta(
+      //     this.formularioConCuenta.get('codigo')?.value.substring(0, 4)
+      //   ),
+      // ])
+      //   .pipe(
+      //     map((respuesta) => {
+      //       let errores = false;
+      //       if (respuesta[0].registros.length > 0) {
+      //         this.formularioConCuenta.get('cuenta_clase')?.setErrors(null);
+      //       } else {
+      //         errores = true;
+      //         this.formularioConCuenta
+      //           .get('cuenta_clase')
+      //           ?.setErrors({ grupoNoValido: true });
+      //         this.changeDetectorRef.detectChanges();
+      //       }
+      //       if (respuesta[1].registros.length > 0) {
+      //         this.formularioConCuenta.get('cuenta_grupo')?.setErrors(null);
+      //       } else {
+      //         errores = true;
+      //         this.formularioConCuenta
+      //           .get('cuenta_grupo')
+      //           ?.setErrors({ grupoNoValido: true });
+      //         this.changeDetectorRef.detectChanges();
+      //       }
+      //       if (respuesta[2].registros.length > 0) {
+      //         this.formularioConCuenta.get('cuenta_subcuenta')?.setErrors(null);
+      //       } else {
+      //         errores = true;
+      //         this.formularioConCuenta
+      //           .get('cuenta_subcuenta')
+      //           ?.setErrors({ grupoNoValido: true });
+      //         this.changeDetectorRef.detectChanges();
+      //       }
+      //       return errores;
+      //     }),
+      //     switchMap((respuestaErrores) => {
+      //       if (respuestaErrores === false) {
+      //         if (this.detalle) {
+      //           return this.cuentaService.actualizarDatos(
+      //             this.detalle,
+      //             this.formularioConCuenta.value
+      //           );
+      //         } else {
+      //           return this.cuentaService.guardarCuenta(
+      //             this.formularioConCuenta.value
+      //           );
+      //         }
+      //       } else {
+      //         return of(null);
+      //       }
+      //     }),
+      //     tap((respuesta) => {
+      //       if (respuesta) {
+      //         if (this.detalle) {
+      //           this.alertaService.mensajaExitoso(
+      //             'Se actualizó la información'
+      //           );
+      //         } else {
+      //           this.alertaService.mensajaExitoso(
+      //             'Se actualizó la información'
+      //           );
+      //         }
+      //         this.activatedRoute.queryParams.subscribe((parametro) => {
+      //           this.router.navigate([`/administrador/detalle`], {
+      //             queryParams: {
+      //               ...parametro,
+      //               detalle: respuesta?.id,
+      //             },
+      //           });
+      //         });
+      //       }
+      //     })
+      //   )
+      //   .subscribe();
     } else {
       this.formularioConCuenta.markAllAsTouched(); // Marcar todos los campos como tocados
     }
@@ -295,5 +314,55 @@ export default class ItemFormularioComponent extends General implements OnInit {
       filtros: [{ propiedad: 'id', valor1: subClase }],
       modelo: 'ConCuentaSubcuenta',
     });
+  }
+
+  private _consultarInformacionInicial() {
+    this._consultarCuentaClaseLista();
+    this._consultarCuentaGrupoLista();
+    this._consultarCuentaCuentaLista();
+  }
+
+  private _consultarCuentaClaseLista() {
+    this._generalService
+      .consultarDatosAutoCompletar<RegistroAutocompletarConCuentaClase>({
+        filtros: [],
+        modelo: 'ConCuentaClase',
+      })
+      .subscribe((response) => {
+        this.cuentaClaseLista = response.registros;
+      });
+  }
+
+  private _consultarCuentaGrupoLista() {
+    this._generalService
+      .consultarDatosAutoCompletar<RegistroAutocompletarConCuentaGrupo>({
+        filtros: [],
+        modelo: 'ConCuentaGrupo',
+      })
+      .subscribe((response) => {
+        this.cuentaGrupoLista = response.registros;
+      });
+  }
+
+  private _consultarCuentaCuentaLista() {
+    this._generalService
+      .consultarDatosAutoCompletar<RegistroAutocompletarConCuentaCuenta>({
+        filtros: [],
+        modelo: 'ConCuentaCuenta',
+      })
+      .subscribe((response) => {
+        this.cuentaCuentaLista = response.registros;
+      });
+  }
+
+  private _listenFormularioCuentaClaseCampo() {
+    this.formularioConCuenta
+      .get('cuenta_clase')
+      ?.valueChanges.subscribe((value) => {
+        this.formularioConCuenta.patchValue({
+          cuenta_grupo: null,
+          cuenta_cuenta: null,
+        });
+      });
   }
 }
