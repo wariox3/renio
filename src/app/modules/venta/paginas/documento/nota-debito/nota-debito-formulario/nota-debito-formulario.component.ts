e0cf9f12ca46e1f822be597b3ddde2cb205e76d5
sebastian <sebastian.h.piedrahita@gmail.com>
2025-03-17 12:03:43 -0500

    feat: agregar sede a nota debito

--- src/app/modules/venta/paginas/documento/nota-debito/nota-debito-formulario/nota-debito-formulario.component.ts
+++ src/app/modules/venta/paginas/documento/nota-debito/nota-debito-formulario/nota-debito-formulario.component.ts
@@ -30,13 +30,14 @@ import {
   NgbModal,
   NgbNavModule,
 } from '@ng-bootstrap/ng-bootstrap';
-import { asyncScheduler, tap, throttleTime } from 'rxjs';
+import { asyncScheduler, tap, throttleTime, zip } from 'rxjs';
 import { TituloAccionComponent } from '../../../../../../comun/componentes/titulo-accion/titulo-accion.component';
 import ContactoFormulario from '@modulos/general/paginas/contacto/contacto-formulario/contacto-formulario.component';
 import { GeneralService } from '@comun/services/general.service';
 import { RegistroAutocompletarGenDocumentoReferencia } from '@interfaces/comunes/autocompletar/general/gen-documento.interface';
 import { RegistroAutocompletarGenContacto } from '@interfaces/comunes/autocompletar/general/gen-contacto.interface';
 import { ParametrosFiltros } from '@interfaces/comunes/componentes/filtros/parametro-filtros.interface';
+import { RegistroAutocompletarGenSede } from '@interfaces/comunes/autocompletar/general/gen-sede.interface';
 
 @Component({
   selector: 'app-nota-debito-formulario',
@@ -73,6 +74,7 @@ export default class FacturaDetalleComponent
   public mostrarDocumentoReferencia =
     this._formularioFacturaService.mostrarDocumentoReferencia;
   public formularioFactura = this._formularioFacturaService.form;
+  public arrSede: RegistroAutocompletarGenSede[] = [];
 
   informacionFormulario: any;
   active: Number;
@@ -187,7 +189,7 @@ export default class FacturaDetalleComponent
   }
 
   ngOnInit() {
-    // this.consultarInformacion();
+    this._consultarInformacion().subscribe();
     this.active = 1;
     this.mostrarDocumentoReferencia.set(true);
     if (this.parametrosUrl) {
@@ -983,6 +985,38 @@ export default class FacturaDetalleComponent
 
   recibirDocumentoDetalle(documento: DocumentoFacturaRespuesta) {
     this._inicializarFormulario(`${documento.contacto_id}`);
+  }
+
+  private _consultarInformacion() {
+    return zip(
+      this._generalService.consultarDatosAutoCompletar<RegistroAutocompletarGenSede>(
+        {
+          modelo: 'GenSede',
+          serializador: 'ListaAutocompletar',
+        },
+      ),
+    ).pipe(
+      tap((respuesta) => {
+        this.arrSede = respuesta[0].registros;
+        if (!this.detalle) {
+          this._initSugerencias();
+        }
+
+        this.changeDetectorRef.detectChanges();
+      }),
+    );
+  }
+
+  private _initSugerencias() {
+    this._sugerirSede(0);
+  }
+
+  private _sugerirSede(posicion: number) {
+    if (this.arrSede.length > 0) {
+      this.formularioFactura.patchValue({
+        sede: this.arrSede?.[posicion].sede_id,
+      });
+    }
   }
 
   private _limpiarDocumentoReferencia(contactoId: string) {
