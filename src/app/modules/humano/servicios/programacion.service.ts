0770f488ceafbb32117b35758e044f6af38e4b33
sebastian <sebastian.h.piedrahita@gmail.com>
2025-03-17 11:13:04 -0500

    agregar boton de desaprobar

--- src/app/modules/humano/servicios/programacion.service.ts
+++ src/app/modules/humano/servicios/programacion.service.ts
@@ -17,35 +17,47 @@ export class ProgramacionService extends Subdominio {
   }
 
   guardarProgramacion(data: any) {
-    return this.httpService.post<ProgramacionRespuesta>(`humano/programacion/`, data);
+    return this.httpService.post<ProgramacionRespuesta>(
+      `humano/programacion/`,
+      data,
+    );
   }
 
   consultarDetalle(id: number) {
-    return this.httpService.getDetalle<ProgramacionRespuesta>(`humano/programacion/${id}/`);
+    return this.httpService.getDetalle<ProgramacionRespuesta>(
+      `humano/programacion/${id}/`,
+    );
   }
 
   actualizarDatosProgramacion(id: number, data: any) {
-    return this.httpService.put<ProgramacionRespuesta>(`humano/programacion/${id}/`, data);
+    return this.httpService.put<ProgramacionRespuesta>(
+      `humano/programacion/${id}/`,
+      data,
+    );
   }
 
   cargarContratos(data: any) {
     return this.httpService.post<CargarContratosRespuesta>(
       `humano/programacion/cargar-contrato/`,
-      data
+      data,
     );
+  }
+
+  desaprobar(data: any) {
+    return this.httpService.post<any>(`humano/programacion/desaprobar/`, data);
   }
 
   generar(data: { id: number }) {
     return this.httpService.post<GenerarProgramacionRespuesta>(
       `humano/programacion/generar/`,
-      data
+      data,
     );
   }
 
   desgenerar(data: { id: number }) {
     return this.httpService.post<DesgenerarProgramacionRespuesta>(
       `humano/programacion/desgenerar/`,
-      data
+      data,
     );
   }
 }
