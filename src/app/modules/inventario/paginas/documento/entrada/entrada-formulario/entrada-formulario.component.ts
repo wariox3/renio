b3a4c9513cfade8d381eeb7506a68012059711ae
sebastian <sebastian.h.piedrahita@gmail.com>
2025-05-13 09:18:04 -0500

    fix: entrada carga item con costo y no precio

--- src/app/modules/inventario/paginas/documento/entrada/entrada-formulario/entrada-formulario.component.ts
+++ src/app/modules/inventario/paginas/documento/entrada/entrada-formulario/entrada-formulario.component.ts
@@ -240,7 +240,7 @@ export default class EntradaFormularioComponent
     indexFormulario: number,
   ) {
     this.detalles.controls[indexFormulario].patchValue({
-      precio: item.precio,
+      precio: item.costo,
       item: item.id,
       cantidad: 1,
       item_nombre: item.nombre,
