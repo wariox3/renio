1bb87f130d76f525d676113011e19111c8918079
sebastian <sebastian.h.piedrahita@gmail.com>
2025-05-31 11:53:33 -0500

    feat: cards de inicio

--- src/app/modules/general/paginas/inicio/inicio-general/inicio-general.component.ts
+++ src/app/modules/general/paginas/inicio/inicio-general/inicio-general.component.ts
@@ -15,6 +15,10 @@ import {
   ApexStroke,
   ApexGrid,
   NgApexchartsModule,
+  ApexNonAxisChartSeries,
+  ApexPlotOptions,
+  ApexResponsive,
+  ApexLegend
 } from 'ng-apexcharts';
 import { zip } from 'rxjs';
 import { dashboardService } from '@modulos/general/servicios/dashboard.service';
@@ -40,6 +44,10 @@ export type ChartOptions = {
   styleUrl: './inicio-general.component.scss',
 })
 export class InicioGeneralComponent extends General implements OnInit {
+  @ViewChild('donutChart') donutChart: ChartComponent;
+  @ViewChild('donutChartPagar') donutChartPagar: ChartComponent;
+  public donutChartOptions: any;
+  public donutChartPagarOptions: any;
   @ViewChild('chart') chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
   ruta = localStorage.getItem('ruta')!;
@@ -73,7 +81,77 @@ export class InicioGeneralComponent extends General implements OnInit {
 
   ngOnInit() {
     this.initializeChart();
+    this.initializeDonutChart();
+    this.initializeDonutChartPagar();
     this.consultarInformacionDashboard();
+  }
+
+  initializeDonutChartPagar() {
+    this.donutChartPagarOptions = {
+      series: [1],
+      chart: {
+        type: 'donut',
+        height: 70,
+        width: 70,
+        sparkline: {
+          enabled: true
+        }
+      },
+      colors: ['#E4E6EF'],
+      stroke: {
+        width: 2
+      },
+      tooltip: {
+        enabled: false
+      },
+      legend: {
+        show: false
+      },
+      dataLabels: {
+        enabled: false
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            size: '50%'
+          }
+        }
+      }
+    };
+  }
+
+  initializeDonutChart() {
+    this.donutChartOptions = {
+      series: [1],
+      chart: {
+        type: 'donut',
+        height: 70,
+        width: 70,
+        sparkline: {
+          enabled: true
+        }
+      },
+      colors: ['#E4E6EF'],
+      stroke: {
+        width: 2
+      },
+      tooltip: {
+        enabled: false
+      },
+      legend: {
+        show: false
+      },
+      dataLabels: {
+        enabled: false
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            size: '50%'
+          }
+        }
+      }
+    };
   }
 
   consultarInformacionDashboard() {
@@ -83,6 +161,36 @@ export class InicioGeneralComponent extends General implements OnInit {
     ).subscribe((respuesta) => {
       this.arrResumenCobrar = respuesta[0];
       this.arrResumenPagar = respuesta[1];
+
+      // Update donut charts data
+      if (this.donutChartOptions) {
+        const totalCobrar = this.arrResumenCobrar.resumen.saldo_pendiente + this.arrResumenCobrar.vencido.saldo_pendiente;
+        if (totalCobrar > 0) {
+          this.donutChartOptions.series = [
+            this.arrResumenCobrar.resumen.saldo_pendiente,
+            this.arrResumenCobrar.vencido.saldo_pendiente
+          ];
+          this.donutChartOptions.colors = ['#50CD89', '#FF0000'];
+        } else {
+          this.donutChartOptions.series = [1];
+          this.donutChartOptions.colors = ['#E4E6EF'];
+        }
+      }
+      
+      if (this.donutChartPagarOptions) {
+        const totalPagar = this.arrResumenPagar.resumen.saldo_pendiente + this.arrResumenPagar.vencido.saldo_pendiente;
+        if (totalPagar > 0) {
+          this.donutChartPagarOptions.series = [
+            this.arrResumenPagar.resumen.saldo_pendiente,
+            this.arrResumenPagar.vencido.saldo_pendiente
+          ];
+          this.donutChartPagarOptions.colors = ['#50CD89', '#FF0000'];
+        } else {
+          this.donutChartPagarOptions.series = [1];
+          this.donutChartPagarOptions.colors = ['#E4E6EF'];
+        }
+      }
+      
       this.changeDetectorRef.detectChanges();
     });
   }
