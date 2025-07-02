23dbe9980b75886c17a333eda9b60b37a97b654f
afcanop <afcanop@gmail.com>
2025-07-02 15:58:46 -0500

    fix: nota de ajuste

--- src/app/modules/compra/paginas/documento/nota-ajuste/nota-ajuste-formulario/nota-ajuste-formulario.component.ts
+++ src/app/modules/compra/paginas/documento/nota-ajuste/nota-ajuste-formulario/nota-ajuste-formulario.component.ts
@@ -165,21 +165,15 @@ export default class FacturaDetalleComponent
 
   consultarInformacion() {
     zip(
-      this._generalService.consultarDatosAutoCompletar<RegistroAutocompletarGenMetodoPago>(
-        {
-          modelo: 'GenMetodoPago',
-          serializador: 'ListaAutocompletar',
-        },
+      this._generalService.consultaApi<RegistroAutocompletarGenMetodoPago>(
+        'general/metodo_pago/seleccionar/',
       ),
-      this._generalService.consultarDatosAutoCompletar<RegistroAutocompletarGenPlazoPago>(
-        {
-          modelo: 'GenPlazoPago',
-          serializador: 'ListaAutocompletar',
-        },
+      this._generalService.consultaApi<RegistroAutocompletarGenPlazoPago>(
+        'general/plazo_pago/seleccionar/',
       ),
     ).subscribe((respuesta: any) => {
-      this.arrMetodosPago = respuesta[0].registros;
-      this.arrPlazoPago = respuesta[1].registros;
+      this.arrMetodosPago = respuesta[0];
+      this.arrPlazoPago = respuesta[1];
       this.changeDetectorRef.detectChanges();
     });
   }
@@ -250,19 +244,12 @@ export default class FacturaDetalleComponent
   }
 
   actualizarFormulario(dato: any, campo: string) {
-    if (campo === 'contacto') {
+    if (campo === 'contacto' || campo === 'documento_referencia') {
       this._inicializarFormulario(dato.id);
       this._limpiarDocumentoReferencia(dato.id);
       this.formularioFactura.get(campo)?.setValue(dato.id);
       this.formularioFactura.get('contactoNombre')?.setValue(dato.nombre_corto);
     }
-    if (campo === 'documento_referencia') {
-      this.formularioFactura.get(campo)?.setValue(dato.id);
-      this.formularioFactura
-        .get('documento_referencia_numero')
-        ?.setValue(dato.numero);
-    }
-
     this.formularioFactura?.markAsDirty();
     this.formularioFactura?.markAsTouched();
     this.changeDetectorRef.detectChanges();
@@ -354,31 +341,19 @@ export default class FacturaDetalleComponent
   }
 
   consultarCliente(event: any) {
-    let arrFiltros: ParametrosFiltros = {
-      filtros: [
-        {
-          propiedad: 'nombre_corto__icontains',
-          valor1: `${event?.target.value}`,
-        },
-        {
-          propiedad: 'proveedor',
-          valor1: 'True',
-        },
-      ],
-      limite: 10,
-      desplazar: 0,
-      ordenamientos: [],
-      limite_conteo: 10000,
-      modelo: 'GenContacto',
-      serializador: 'ListaAutocompletar',
-    };
-
     this._generalService
-      .consultarDatosAutoCompletar<RegistroAutocompletarGenContacto>(arrFiltros)
+      .consultaApi<RegistroAutocompletarGenContacto>(
+        'general/contacto/seleccionar/',
+        {
+          nombre_corto__icontains: `${event?.target.value}`,
+          proveedor: 'True',
+          limit: 10,
+        },
+      )
       .pipe(
         throttleTime(300, asyncScheduler, { leading: true, trailing: true }),
-        tap((respuesta) => {
-          this.arrMovimientosClientes = respuesta.registros;
+        tap((respuesta: any) => {
+          this.arrMovimientosClientes = respuesta;
           this.changeDetectorRef.detectChanges();
         }),
       )
