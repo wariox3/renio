64bcde1e15a0a7d13ce6172d2b0217a1d8a7e5c6
sebastian <sebastian.h.piedrahita@gmail.com>
2025-07-07 10:47:54 -0500

    feat: mostrar creditos disponibles en la vista de socios

--- src/app/modules/socio/paginas/socio/socio.component.ts
+++ src/app/modules/socio/paginas/socio/socio.component.ts
@@ -1,13 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { General } from '@comun/clases/general';
 import { CardComponent } from '@comun/componentes/card/card.component';
 import { AuthService } from '@modulos/auth';
 import { ContenedorService } from '@modulos/contenedor/servicios/contenedor.service';
 import { ConsultaCredito } from '@modulos/socio/interfaces/socio.interface';
-import { NgbActiveModal, NgbModalModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbActiveModal,
+  NgbModalModule,
+  NgbNavModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
-import { obtenerUsuarioId, obtenerUsuarioSocioId } from '@redux/selectors/usuario.selectors';
+import {
+  obtenerUsuarioId,
+  obtenerUsuarioSocioId,
+  obtenerUsuarioVrcredito,
+} from '@redux/selectors/usuario.selectors';
+import { CountUpModule } from 'ngx-countup';
 import { zip } from 'rxjs';
 
 @Component({
@@ -16,16 +30,27 @@ import { zip } from 'rxjs';
   templateUrl: './socio.component.html',
   styleUrls: ['./socio.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, CardComponent, NgbNavModule, NgbModalModule, TranslateModule],
+  imports: [
+    CommonModule,
+    CardComponent,
+    NgbNavModule,
+    NgbModalModule,
+    TranslateModule,
+    CountUpModule,
+  ],
   providers: [NgbActiveModal],
 })
 export class SocioComponent extends General implements OnInit {
   codigoUsuarioSocioId = '';
-  arrMovimientos:ConsultaCredito[] = [];
-  arrListaSocio:any
+  vrCredito = signal(0);
+  arrMovimientos: ConsultaCredito[] = [];
+  arrListaSocio: any;
   active: number = 1;
 
-  constructor(private contenedorServices: ContenedorService, private authService: AuthService) {
+  constructor(
+    private contenedorServices: ContenedorService,
+    private authService: AuthService,
+  ) {
     super();
   }
 
@@ -33,16 +58,20 @@ export class SocioComponent extends General implements OnInit {
     this.store.select(obtenerUsuarioSocioId).subscribe((codigoUsuarioSocio) => {
       this.codigoUsuarioSocioId = codigoUsuarioSocio;
     });
+    this.store
+      .select(obtenerUsuarioVrcredito)
+      .subscribe((credito) => this.vrCredito.set(credito));
     this.consultarInformacion();
     this.changeDetectorRef.detectChanges();
   }
 
   consultarInformacion() {
-
     zip(
-      this.contenedorServices.consultarMovimientoSocio(this.codigoUsuarioSocioId),
+      this.contenedorServices.consultarMovimientoSocio(
+        this.codigoUsuarioSocioId,
+      ),
       this.authService.listaSocio(this.codigoUsuarioSocioId),
-    ).subscribe((respuesta:any) => {
+    ).subscribe((respuesta: any) => {
       this.arrMovimientos = respuesta[0].movimientos;
       this.arrListaSocio = respuesta[1].usuarios;
       this.changeDetectorRef.detectChanges();
