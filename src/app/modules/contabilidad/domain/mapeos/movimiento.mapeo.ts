2d5939eb9313c3ff8371caf89d38a4322e8f45d4
sebastian <sebastian.h.piedrahita@gmail.com>
2025-07-10 08:43:56 -0500

    fix: cuenta_codigo pasa a cuenta__codigo

--- src/app/modules/contabilidad/domain/mapeos/movimiento.mapeo.ts
+++ src/app/modules/contabilidad/domain/mapeos/movimiento.mapeo.ts
@@ -4,5 +4,5 @@ export const MOVIMIENTO_FILTERS: FilterField[] = [
     { name: 'id', displayName: 'ID', type: 'number' },
     { name: 'numero', displayName: 'Número', type: 'string' },
     { name: 'fecha', displayName: 'Fecha', type: 'date' },
-    { name: 'cuenta_codigo', displayName: 'Cuenta codigo', type: 'string' },
+    { name: 'cuenta__codigo', displayName: 'Cuenta codigo', type: 'string' },
 ];
