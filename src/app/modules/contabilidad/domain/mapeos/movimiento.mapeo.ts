cc7592f340926700c35ed2b87e45944056da0fba
sebastian <sebastian.h.piedrahita@gmail.com>
2025-07-10 08:34:20 -0500

    feat: agregar filtro cuenta_codigo a movimiento

--- src/app/modules/contabilidad/domain/mapeos/movimiento.mapeo.ts
+++ src/app/modules/contabilidad/domain/mapeos/movimiento.mapeo.ts
@@ -4,4 +4,5 @@ export const MOVIMIENTO_FILTERS: FilterField[] = [
     { name: 'id', displayName: 'ID', type: 'number' },
     { name: 'numero', displayName: 'Número', type: 'string' },
     { name: 'fecha', displayName: 'Fecha', type: 'date' },
+    { name: 'cuenta_codigo', displayName: 'Cuenta codigo', type: 'string' },
 ];
