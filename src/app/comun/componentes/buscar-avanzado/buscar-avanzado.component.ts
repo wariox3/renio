0a1c2d96156d5d11702d652faa1d0b9e4efa5933
afcanop <afcanop@gmail.com>
2025-07-10 10:48:42 -0500

    feat> ajuste en buscar avanzado en nota credito

--- src/app/comun/componentes/buscar-avanzado/buscar-avanzado.component.ts
+++ src/app/comun/componentes/buscar-avanzado/buscar-avanzado.component.ts
@@ -1,9 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { General } from '@comun/clases/general';
-import { mapeo } from '@comun/extra/mapeo-entidades/buscar-avanzados';
-import { HttpService } from '@comun/services/http.service';
-import { CampoLista } from '@interfaces/comunes/componentes/buscar-avanzado/buscar-avanzado.interface';
+import { GeneralService } from '@comun/services/general.service';
 import {
   NgbDatepickerModule,
   NgbModal,
@@ -11,10 +9,10 @@ import {
 } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { KeysPipe } from '@pipe/keys.pipe';
-import { ActualizarMapeo } from '@redux/actions/menu.actions';
 import { BaseFiltroComponent } from '../base-filtro/base-filtro.component';
-import { Modelo } from '@comun/type/modelo.type';
-import { MapeoDocumentos } from '@comun/type/mapeo-documentos.type';
+import { FiltroComponent } from '../ui/tabla/filtro/filtro.component';
+import { CampoLista } from '@interfaces/comunes/componentes/buscar-avanzado/buscar-avanzado.interface';
+import { FilterTransformerService } from 'src/app/core/services/filter-transformer.service';
 
 @Component({
   selector: 'app-comun-buscar-avanzado',
@@ -26,44 +24,35 @@ import { MapeoDocumentos } from '@comun/type/mapeo-documentos.type';
     BaseFiltroComponent,
     TranslateModule,
     NgbTooltipModule,
+    FiltroComponent,
   ],
   templateUrl: './buscar-avanzado.component.html',
   styleUrls: ['./buscar-avanzado.component.scss'],
 })
 export class BuscarAvanzadoComponent extends General {
-  closeResult = '';
-  arrPropiedades: any;
-  ordenadoTabla: string = '';
-  arrItems: any[];
-  @Input() consultarModelo = '';
-  @Input() campoLista: CampoLista[] = [];
+  private _generalService = inject(GeneralService);
+  private _filterTransformerService = inject(FilterTransformerService);
+  public closeResult = '';
+  public arrPropiedades: any;
+  public ordenadoTabla: string = '';
+  public arrItems: any[];
+  public filtros = {};
+  @Input() consultarUrl = '';
+  @Input() tituloModal = '';
+  @Input() consultarModelo = ''; //BORRAR
+  @Input() campoLista: any[] = [];
   @Input() campoFiltros: string[] = [];
   @Input() filtrosPermanentes: any = {};
-  @Input() consultarTipo: 'Administrador' | 'Documento';
+  @Input() consultarTipo: 'Administrador' | 'Documento'; //borrar
   @Output() emitirRegistroSeleccionado: EventEmitter<any> = new EventEmitter();
 
-  arrParametrosConsulta: any = {
-    filtros: [],
-    limite: 50,
-    desplazar: 0,
-    ordenamientos: [],
-    limite_conteo: 10000,
-    modelo: '',
-    tipo: '',
-  };
-
-  constructor(
-    private modalService: NgbModal,
-    private httpService: HttpService
-  ) {
+  constructor(private modalService: NgbModal) {
     super();
   }
 
   abirModal(content: any) {
-    this.arrParametrosConsulta.filtros = [];
+    this._reiniciarFiltros();
     this.consultarLista();
-    let posicion: keyof typeof mapeo = this.consultarModelo as Modelo;
-    this.store.dispatch(ActualizarMapeo({ dataMapeo: mapeo[posicion] }));
     this.modalService.open(content, {
       ariaLabelledBy: 'modal-basic-title',
       size: 'lg',
@@ -72,56 +61,31 @@ export class BuscarAvanzadoComponent extends General {
   }
 
   obtenerFiltros(arrfiltros: any) {
-    this.arrParametrosConsulta.filtros = [...arrfiltros];
+    this.filtros =
+      this._filterTransformerService.transformToApiParams(arrfiltros);
     this.consultarLista();
   }
 
   consultarLista() {
-    this.arrParametrosConsulta.modelo = this.consultarModelo;
-    this.arrParametrosConsulta.tipo = this.consultarTipo;
-    let baseUrl = 'general/';
-    switch (this.consultarTipo) {
-      case 'Administrador':
-        baseUrl += 'funcionalidad/lista/';
-        break;
-      case 'Documento':
-        baseUrl += 'funcionalidad/lista/';
-        break;
-    }
-
-    if (Array.isArray(this.filtrosPermanentes)) {
-      this.arrParametrosConsulta.filtros = [
-        ...this.arrParametrosConsulta.filtros,
+    this._generalService
+      .consultaApi(this.consultarUrl, {
+        ...this.filtros,
         ...this.filtrosPermanentes,
-      ];
-    } else {
-      if (Object.keys(this.filtrosPermanentes).length > 0) {
-        this.arrParametrosConsulta.filtros[0] = this.filtrosPermanentes;
-      }
-    }
-
-    this.httpService
-      .post<{
-        cantidad_registros: number;
-        registros: any[];
-        propiedades: any[];
-      }>(baseUrl, this.arrParametrosConsulta)
-      .subscribe((respuesta) => {
+      })
+      .subscribe((respuesta: any) => {
         // Mapea cada registro en respuesta.registros para crear un nuevo array this.arrItems
-        this.arrItems = respuesta.registros.map((registro) => {
+        this.arrItems = respuesta.results.map((registro: any) => {
           // Inicializa un objeto vacío para almacenar los valores de los campos especificados
           let valores: any[] = [];
           // Itera sobre cada campo en this.campoLista
           this.campoLista.forEach((campo) => {
             // Si el campo existe en el registro, agrégalo al objeto valores
-            if (registro[campo.propiedad] !== undefined) {
-               valores.push(
-                {
-                  ...campo,
-                  valor: registro[campo.propiedad],
-                  ...registro
-                }
-               )
+            if (registro[campo.name] !== undefined) {
+              valores.push({
+                ...campo,
+                valor: registro[campo.name],
+                ...registro,
+              });
             }
           });
 
@@ -146,17 +110,13 @@ export class BuscarAvanzadoComponent extends General {
     this.emitirRegistroSeleccionado.emit(item[index][0]);
   }
 
-
   // Función para determinar el tipo de dato y aplicar formato si es necesario
   getTipoDato(campo: any) {
     // Verifica si se proporciona un campo
     if (campo) {
       // Switch para manejar diferentes tipos de campo
       switch (campo.campoTipo) {
-        // Si el campo es FloatField o IntegerField
-        case 'FloatField':
-        case 'IntegerField':
-          // Verifica si se debe aplicar formato numérico
+        case 'number':
           if (campo.aplicaFormatoNumerico) {
             // Formatea el valor con dos decimales y comas para separar miles
             let formattedValue = campo.valor
@@ -165,28 +125,26 @@ export class BuscarAvanzadoComponent extends General {
 
             return `${formattedValue}`;
           }
-          // Si no se aplica formato numérico, devuelve el valor sin modificar
           return campo.valor;
         // Si el campo es de tipo booleano
         case 'Booleano':
-          // Convierte el valor booleano a 'Si' si es verdadero y a 'No' si es falso
           return campo.valor ? 'SI' : 'NO';
         case 'Porcentaje':
-          // Verifica si `valor` es una cadena antes de aplicar `replace`
           if (typeof campo.valor === 'string') {
             return `${parseFloat(campo.valor.replace(',', '.'))}`;
           } else if (campo.valor !== null && campo.valor !== undefined) {
             // Convierte `valor` a cadena si no es null o undefined
             return `${parseFloat(String(campo.valor).replace(',', '.'))}`;
           } else {
-            // Maneja el caso cuando `valor` es null o undefined
             return '0'; // O algún valor predeterminado adecuado
           }
-        // En caso de que el tipo de campo no sea ninguno de los anteriores
         default:
-          // Devuelve el valor sin modificar
           return campo.valor;
       }
     }
+  }
+
+  private _reiniciarFiltros() {
+    this.filtros = {};
   }
 }
--- src/app/modules/general/domain/mapeos/contacto.mapeo.ts
+++ src/app/modules/general/domain/mapeos/contacto.mapeo.ts
@@ -1,10 +1,31 @@
-import { FilterField } from "src/app/core/interfaces/filtro.interface";
+import {
+  FilterCondition,
+  FilterField,
+} from 'src/app/core/interfaces/filtro.interface';
 
 export const CONTACTO_FILTERS: FilterField[] = [
-    { name: 'id', displayName: 'ID', type: 'number' },
-    { name: 'nombre_corto', displayName: 'Nombre', type: 'string' },
-    { name: 'numero_identificacion', displayName: 'Número de identificación', type: 'string' },
-    { name: 'cliente', displayName: 'Cliente', type: 'boolean' },
-    { name: 'proveedor', displayName: 'Proveedor', type: 'boolean' },
-    { name: 'empleado', displayName: 'Empleado', type: 'boolean' },
+  { name: 'id', displayName: 'ID', type: 'number' },
+  { name: 'nombre_corto', displayName: 'Nombre', type: 'string' },
+  {
+    name: 'numero_identificacion',
+    displayName: 'Número de identificación',
+    type: 'string',
+  },
+  { name: 'cliente', displayName: 'Cliente', type: 'boolean' },
+  { name: 'proveedor', displayName: 'Proveedor', type: 'boolean' },
+  { name: 'empleado', displayName: 'Empleado', type: 'boolean' },
 ];
+
+export const CONTACTO_LISTA_BUSCAR_AVANZADO: FilterField[] = [
+  { name: 'id', displayName: 'ID', type: 'number' },
+  {
+    name: 'numero_identificacion',
+    displayName: 'Número de identificación',
+    type: 'string',
+  },
+  { name: 'nombre_corto', displayName: 'Nombre', type: 'string' },
+];
+
+export const CONTACTO_FILTRO_PERMANENTE_CLIENTE: Record<string, any> = {
+  cliente: 'True',
+};
