bfea15bdd8da7eff2805213bb886da262ecd2f01
sebastian <sebastian.h.piedrahita@gmail.com>
2025-07-11 11:54:18 -0500

    feat: cambio de organizacion tabla liquidacion detalle

--- src/app/modules/humano/interfaces/liquidacion.interface.ts
+++ src/app/modules/humano/interfaces/liquidacion.interface.ts
@@ -1,18 +1,23 @@
 export interface Liquidacion {
-    id: number;
-    fecha: string;
-    fecha_desde: string;
-    fecha_hasta: string;
-    contrato: number;
-    dias: number;
-    cesantia: number;
-    interes: number;
-    prima: number;
-    vacacion: number;
-    deduccion: number;
-    adicion: number;
-    total: number;
-    estado_aprobado: boolean;
-    estado_generado: boolean;
-    comentario: string | null;
+    id: number
+    fecha: string
+    contrato__contacto__numero_identificacion: string
+    contrato__contacto__nombre_corto: string
+    contrato_id: number
+    fecha_desde: string
+    fecha_hasta: string
+    total: number
+    dias: number
+    dias_cesantia: number
+    dias_prima: number
+    dias_vacacion: number
+    cesantia: number
+    interes: number
+    prima: number
+    vacacion: number
+    deduccion: number
+    adicion: number
+    estado_generado: boolean
+    estado_aprobado: boolean
+    comentario: any
 }
