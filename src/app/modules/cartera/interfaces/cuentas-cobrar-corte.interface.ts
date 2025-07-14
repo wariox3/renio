b1ac0e5fdaca605d8d580d47603945e86d2aa0d7
sebastian <sebastian.h.piedrahita@gmail.com>
2025-07-14 17:40:12 -0500

    feat: cuenta cobrar corte por generar

--- src/app/modules/cartera/interfaces/cuentas-cobrar-corte.interface.ts
+++ src/app/modules/cartera/interfaces/cuentas-cobrar-corte.interface.ts
@@ -1,15 +1,15 @@
 export interface CuentasCobrarCorte {
-  id: number;
-  numero: number;
-  fecha: string;
-  fecha_vence: string;
-  documento_tipo_id: number;
-  subtotal: number;
-  impuesto: number;
-  total: number;
-  abono: number;
-  saldo: number;
-  documento_tipo_nombre: string;
-  contacto_numero_identificacion: string;
-  contacto_nombre_corto: string;
+    id: number
+    numero: number
+    fecha: string
+    fecha_vence: string
+    documento_tipo_id: number
+    subtotal: number
+    impuesto: number
+    total: number
+    abono: number
+    saldo: number
+    documento_tipo__nombre: string
+    contacto__numero_identificacion: string
+    contacto__nombre_corto: string 
 }
