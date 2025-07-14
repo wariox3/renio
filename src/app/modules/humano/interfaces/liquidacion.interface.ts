be3f5781d9e9ecfc389585da73da5b59fd34f28a
sebastian <sebastian.h.piedrahita@gmail.com>
2025-07-14 11:31:44 -0500

    feat: agregar campos

--- src/app/modules/humano/interfaces/liquidacion.interface.ts
+++ src/app/modules/humano/interfaces/liquidacion.interface.ts
@@ -1,23 +1,27 @@
 export interface Liquidacion {
-    id: number
-    fecha: string
-    contrato__contacto__numero_identificacion: string
-    contrato__contacto__nombre_corto: string
-    contrato_id: number
-    fecha_desde: string
-    fecha_hasta: string
-    total: number
-    dias: number
-    dias_cesantia: number
-    dias_prima: number
-    dias_vacacion: number
-    cesantia: number
-    interes: number
-    prima: number
-    vacacion: number
-    deduccion: number
-    adicion: number
-    estado_generado: boolean
-    estado_aprobado: boolean
-    comentario: any
+  id: number;
+  fecha: string;
+  contrato__contacto__numero_identificacion: string;
+  contrato__contacto__nombre_corto: string;
+  contrato_id: number;
+  fecha_desde: string;
+  fecha_hasta: string;
+  total: number;
+  dias: number;
+  dias_cesantia: number;
+  dias_prima: number;
+  dias_vacacion: number;
+  fecha_ultimo_pago: string | null;
+  fecha_ultimo_pago_prima: string | null;
+  fecha_ultimo_pago_cesantia: string | null;
+  fecha_ultimo_pago_vacacion: string | null;
+  cesantia: number;
+  interes: number;
+  prima: number;
+  vacacion: number;
+  deduccion: number;
+  adicion: number;
+  estado_generado: boolean;
+  estado_aprobado: boolean;
+  comentario: any;
 }
