853e5e08a2c175e80f7a12c4f354eb03bd284f4f
sebas957 <baxtian957@hotmail.com>
2025-07-29 17:55:37 -0500

    AJustes imprimir historial facturacion

--- src/app/comun/services/http.service.ts
+++ src/app/comun/services/http.service.ts
@@ -165,40 +165,95 @@ export class HttpService extends Subdominio {
     this.alertaService.mensajaEspera('Cargando');
     this.http
       .post<HttpResponse<Blob>>(url, data, {
-        // observe: 'response',
-        // responseType: 'blob' as 'json',
+        observe: 'response',
+        responseType: 'blob' as 'json',
       })
+      .pipe(
+        catchError((error) => {
+          this.alertaService.cerrarMensajes();
+          this.alertaService.mensajeError(
+            `Error 15`,
+            'El documento no tiene un formato'
+          );
+          return of(null);
+        })
+      )
       .subscribe((response) => {
         if (response !== null) {
           const headers = response.headers as HttpHeaders;
+          if (headers.get('Content-Disposition') !== null) {
+            let nombreArchivo = headers
+              .get('Content-Disposition')!
+              .split(';')[1]
+              .trim()
+              .split('=')[1];
+            nombreArchivo = decodeURI(nombreArchivo.replace(/"/g, ''));
 
-          let nombreArchivo = headers
-            .get('Content-Disposition')!
-            .split(';')[1]
-            .trim()
-            .split('=')[1];
-          nombreArchivo = decodeURI(nombreArchivo.replace(/"/g, ''));
+            if (!nombreArchivo) {
+              throw new Error('fileName error')
+            }
+            const data: any = response.body;
 
-          if (!nombreArchivo) {
-            console.log('fileName error');
-            return;
-          }
-          const data: any = response.body;
-
-          if (data !== null) {
-            const blob = new Blob([data], {
-              type: data?.type,
-            });
-            const objectUrl = URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.setAttribute('style', 'display:none');
-            a.setAttribute('href', objectUrl);
-            a.setAttribute('download', nombreArchivo);
-            a.click();
-            URL.revokeObjectURL(objectUrl);
+            if (data !== null) {
+              const blob = new Blob([data], {
+                type: data?.type,
+              });
+              const objectUrl = URL.createObjectURL(blob);
+              const a = document.createElement('a');
+              a.setAttribute('style', 'display:none');
+              a.setAttribute('href', objectUrl);
+              a.setAttribute('download', nombreArchivo);
+              a.click();
+              URL.revokeObjectURL(objectUrl);
+              setTimeout(() => this.alertaService.cerrarMensajes(), 1000);
+            }
+          } else {
             setTimeout(() => this.alertaService.cerrarMensajes(), 1000);
+            throw new Error('Error no existe Content-Disposition')
           }
         }
       });
   }
+
+  // public descargarArchivoDominio(endpoint: string, data: any): void {
+  //   const url = `${this.URL_API_BASE}/${endpoint}`;
+  //   this.alertaService.mensajaEspera('Cargando');
+  //   this.http
+  //     .post<HttpResponse<Blob>>(url, data, {
+  //         observe: 'response',
+  //         responseType: 'blob' as 'json',
+  //     })
+  //     .subscribe((response) => {
+  //       if (response !== null) {
+  //         const headers = response.headers as HttpHeaders;
+
+  //         let nombreArchivo = headers
+  //           .get('Content-Disposition')!
+  //           .split(';')[1]
+  //           .trim()
+  //           .split('=')[1];
+  //         nombreArchivo = decodeURI(nombreArchivo.replace(/"/g, ''));
+
+  //         if (!nombreArchivo) {
+  //           console.log('fileName error');
+  //           return;
+  //         }
+  //         const data: any = response.body;
+
+  //         if (data !== null) {
+  //           const blob = new Blob([data], {
+  //             type: data?.type,
+  //           });
+  //           const objectUrl = URL.createObjectURL(blob);
+  //           const a = document.createElement('a');
+  //           a.setAttribute('style', 'display:none');
+  //           a.setAttribute('href', objectUrl);
+  //           a.setAttribute('download', nombreArchivo);
+  //           a.click();
+  //           URL.revokeObjectURL(objectUrl);
+  //           setTimeout(() => this.alertaService.cerrarMensajes(), 1000);
+  //         }
+  //       }
+  //     });
+  // }
 }
