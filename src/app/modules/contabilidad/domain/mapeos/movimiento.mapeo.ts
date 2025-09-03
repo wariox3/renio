2641f32d9e23b276ce5f97bdc39213f54be7d340
afcanop <afcanop@gmail.com>
2025-09-03 15:53:45 -0500

    feat: agregar nuevos filtros para grupo en MOVIMIENTO_FILTERS

--- src/app/modules/contabilidad/domain/mapeos/movimiento.mapeo.ts
+++ src/app/modules/contabilidad/domain/mapeos/movimiento.mapeo.ts
@@ -4,7 +4,9 @@ export const MOVIMIENTO_FILTERS: FilterField[] = [
     { name: 'id', displayName: 'ID', type: 'number' },
     { name: 'numero', displayName: 'Número', type: 'string' },
     { name: 'fecha', displayName: 'Fecha', type: 'date' },
-    { name: 'cuenta__codigo', displayName: 'Cuenta codigo', type: 'string' },
+    { name: 'cuenta__codigo', displayName: 'Cuenta código', type: 'string' },
+    { name: 'grupo__codigo', displayName: '[Grupo] código', type: 'string' },
+    { name: 'grupo__nombre', displayName: '[Grupo] nombre', type: 'string' },
     { name: 'contacto__nombre_corto', displayName: '[Contacto] nombre', type: 'string'},
     { name: 'contacto__numero_identificacion', displayName: '[Contacto] identificación', type: 'string'},
 ];
