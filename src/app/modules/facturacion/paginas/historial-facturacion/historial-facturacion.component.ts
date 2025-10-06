f9aee0d252c2e5d7eab597c34d18a538a1daf777
Sebastian <sebastian.h.piedrahita@gmail.com>
2025-10-06 09:12:17 -0500

    feat: add pagination to billing history table

--- src/app/modules/facturacion/paginas/historial-facturacion/historial-facturacion.component.ts
+++ src/app/modules/facturacion/paginas/historial-facturacion/historial-facturacion.component.ts
@@ -1,6 +1,6 @@
 import { ContenedorService } from '@modulos/contenedor/servicios/contenedor.service';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { General } from '@comun/clases/general';
 import { obtenerUsuarioId } from '@redux/selectors/usuario.selectors';
 import { switchMap, tap } from 'rxjs';
@@ -8,16 +8,20 @@ import { Movimiento } from '@interfaces/facturacion/Facturacion';
 import { TranslateModule } from '@ngx-translate/core';
 import { HttpService } from '@comun/services/http.service';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { PaginadorComponent } from '@comun/componentes/ui/tabla/paginador/paginador.component';
 
 @Component({
   selector: 'app-historial-facturacion',
   standalone: true,
   templateUrl: './historial-facturacion.component.html',
   styleUrls: ['./historial-facturacion.component.scss'],
-  imports: [CommonModule, TranslateModule, NgbTooltipModule],
+  imports: [CommonModule, TranslateModule, NgbTooltipModule, PaginadorComponent],
 })
 export class HistorialFacturacionComponent extends General implements OnInit {
   movientos: Movimiento[];
+  currentPage = signal<number>(1);
+  totalItems: number = 0;
+  usuarioId: number;
 
   constructor(private contenedorService: ContenedorService, private httpService: HttpService) {
     super();
@@ -28,10 +32,14 @@ export class HistorialFacturacionComponent extends General implements OnInit {
       .select(obtenerUsuarioId)
       .pipe(
         switchMap((respuestaUsuarioId) =>
-          this.contenedorService.consultaUsuario(respuestaUsuarioId)
+        {
+          this.usuarioId = respuestaUsuarioId;
+          return this.contenedorService.consultaUsuario(respuestaUsuarioId)
+        }
         ),
         tap((respuestaConsultaUsuario) => {
           this.movientos = respuestaConsultaUsuario.movimientos;
+          this.totalItems = respuestaConsultaUsuario.movimientos?.length || 0;
           this.changeDetectorRef.detectChanges();
         })
       )
@@ -43,5 +51,18 @@ export class HistorialFacturacionComponent extends General implements OnInit {
     this.httpService.descargarArchivoDominio('contenedor/movimiento/descargar/', {
       'id': documento_id
     });
+  }
+
+  consultarMovimientos(usuarioId: number) {
+    this.contenedorService.consultaUsuario(usuarioId).subscribe((respuestaConsultaUsuario) => {
+      this.movientos = respuestaConsultaUsuario.movimientos;
+      this.totalItems = respuestaConsultaUsuario.movimientos?.length || 0;
+      this.changeDetectorRef.detectChanges();
+    });
+  }
+
+  cambiarPaginacion(page: number) {
+    this.currentPage.set(page);
+    this.consultarMovimientos(this.usuarioId);
   }
 }
