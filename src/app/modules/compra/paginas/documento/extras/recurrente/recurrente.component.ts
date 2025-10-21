fdd96ec9b5cb16815858aafb1595a236ccf50128
Sebastian <sebastian.h.piedrahita@gmail.com>
2025-10-21 10:52:09 -0500

    feat: add ID column and styling to recurring purchase document table

--- src/app/modules/compra/paginas/documento/extras/recurrente/recurrente.component.ts
+++ src/app/modules/compra/paginas/documento/extras/recurrente/recurrente.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { FiltroComponent } from "@comun/componentes/ui/tabla/filtro/filtro.component";
+import { FiltroComponent } from '@comun/componentes/ui/tabla/filtro/filtro.component';
 import { ConfigModuleService } from '@comun/services/application/config-modulo.service';
 import { GeneralService } from '@comun/services/general.service';
 import { HttpService } from '@comun/services/http.service';
@@ -10,9 +10,15 @@ import { ModeloConfig } from '@interfaces/menu/configuracion.interface';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SiNoPipe } from '@pipe/si-no.pipe';
 import { Subject, takeUntil } from 'rxjs';
-import { ParametrosApi, RespuestaApi } from 'src/app/core/interfaces/api.interface';
-import { FilterCondition, FilterField } from 'src/app/core/interfaces/filtro.interface';
-import { PaginadorComponent } from "@comun/componentes/ui/tabla/paginador/paginador.component";
+import {
+  ParametrosApi,
+  RespuestaApi,
+} from 'src/app/core/interfaces/api.interface';
+import {
+  FilterCondition,
+  FilterField,
+} from 'src/app/core/interfaces/filtro.interface';
+import { PaginadorComponent } from '@comun/componentes/ui/tabla/paginador/paginador.component';
 import { FilterTransformerService } from 'src/app/core/services/filter-transformer.service';
 
 @Component({
@@ -20,11 +26,11 @@ import { FilterTransformerService } from 'src/app/core/services/filter-transform
   standalone: true,
   imports: [FiltroComponent, SiNoPipe, CommonModule, PaginadorComponent],
   templateUrl: './recurrente.component.html',
+  styleUrls: ['./recurrente.component.scss'],
 })
 export default class RecurrenteComponent implements OnInit, OnDestroy {
-
   private readonly _generalService = inject(GeneralService);
-  private readonly _httpService  = inject(HttpService);
+  private readonly _httpService = inject(HttpService);
   private readonly _configModuleService = inject(ConfigModuleService);
   private readonly _filterTransformerService = inject(FilterTransformerService);
 
@@ -34,13 +40,13 @@ export default class RecurrenteComponent implements OnInit, OnDestroy {
   private _destroy$ = new Subject<void>();
   public modeloConfig: ModeloConfig | null;
   public currentPage = signal(1);
-  public parametrosConsulta : ParametrosApi = {
+  public parametrosConsulta: ParametrosApi = {
     serializador: 'lista',
     documento_tipo_id: 32,
     ordering: 'estado_aprobado,-fecha,-numero,-id',
-  }
-  public facturas = signal<any>([])
-  public cantidadRegistros = signal<number>(0)
+  };
+  public facturas = signal<any>([]);
+  public cantidadRegistros = signal<number>(0);
   public availableFields: FilterField[] = [];
 
   ngOnInit(): void {
@@ -63,8 +69,19 @@ export default class RecurrenteComponent implements OnInit, OnDestroy {
 
   private _loadModuleConfiguration(modeloConfig: ModeloConfig | null) {
     this._key = modeloConfig?.key;
-        this.availableFields =
-      modeloConfig?.ajustes.parametrosHttpConfig?.filtros?.ui || [];
+    this.availableFields = [
+      { name: 'id', displayName: 'ID', type: 'number' },
+      {
+        name: 'contacto__numero_identificacion',
+        displayName: '[Contacto] Identificación',
+        type: 'string',
+      },
+      {
+        name: 'contacto__nombre_corto',
+        displayName: '[Contacto] Nombre',
+        type: 'string',
+      },
+    ];
   }
 
   private consultarLista() {
@@ -73,23 +90,22 @@ export default class RecurrenteComponent implements OnInit, OnDestroy {
         RespuestaApi<any>
       >('general/documento/', this.parametrosConsulta)
       .subscribe((respuesta) => {
-        this.facturas.set(respuesta.results)
+        this.facturas.set(respuesta.results);
         this.cantidadRegistros.set(respuesta.count);
       });
   }
 
-
   filterChange(filters: FilterCondition[]) {
     // Transformar los filtros a parámetros de API
     const apiParams =
-       this._filterTransformerService.transformToApiParams(filters);
+      this._filterTransformerService.transformToApiParams(filters);
 
     this._generalService
       .consultaApi<
         RespuestaApi<any>
-      >('general/documento/', {...this.parametrosConsulta, ...apiParams})
+      >('general/documento/', { ...this.parametrosConsulta, ...apiParams })
       .subscribe((respuesta) => {
-        this.facturas.set(respuesta.results)
+        this.facturas.set(respuesta.results);
         this.cantidadRegistros.set(respuesta.count);
       });
   }
@@ -103,28 +119,22 @@ export default class RecurrenteComponent implements OnInit, OnDestroy {
     this.consultarLista();
   }
 
-  generar(id: number){
+  generar(id: number) {
     this._httpService
-      .post<
-        RespuestaApi<any>
-      >('general/documento/generar/', {id})
+      .post<{ id: number }>('general/documento/generar/', { id })
       .subscribe({
-        next: (respuesta: any) => {
-            this.router.navigate(
-              [`compra/documento/detalle/${id}`],
-              {
-                queryParams: {
-                  modelo: this._key
-                },
-              },
-            );
-            this._ngbModal.dismissAll()
+        next: (respuesta: { id: number }) => {
+          this.router.navigate([`compra/documento/detalle/${respuesta.id}`], {
+            queryParams: {
+              modelo: this._key,
+            },
+          });
+          this._ngbModal.dismissAll();
         },
         error: (err) => {
-          console.error('Subscriber error:', err)
-          this._ngbModal.dismissAll()
+          console.error('Subscriber error:', err);
+          this._ngbModal.dismissAll();
         }, // This will only be called if you re-throw the error in catchError
-
       });
   }
 }
