fe77ea65935ce3918e74081fb0700a2c4f4da868
afcanop <afcanop@gmail.com>
2025-10-29 13:37:53 -0500

    feat: se crea componente generico de btn

---
+++ src/app/common/components/ui/button/button.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonComponent {
+  @Input() text = '';
+  @Input() loadingText = '';
+  @Input() type: 'submit' | 'button' | 'reset' = 'button';
+  @Input() color:
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'danger'
+    | 'warning'
+    | 'info'
+    | 'light'
+    | 'dark' = 'primary';
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+  @Input() isLoading = false;
+  @Input() isDisabled = false;
+  @Input() iconLeft = '';
+  @Input() iconRight = '';
+  @Input() iconOnly = false;
+  @Input() rounded = false;
+  @Input() outline = false;
+  @Input() block = false;
+  @Input() customClasses = '';
+  @Input() title = ''
+  @Output() buttonClick = new EventEmitter<void>();
+
+  handleClick() {
+    this.buttonClick.emit();
+  }
+
+  /** ✅ Computa las clases dinámicamente */
+  get computedClasses() {
+    return {
+      [`btn-${this.color}`]: !this.outline,
+      [`btn-outline-${this.color}`]: this.outline,
+      'btn-sm': this.size === 'sm',
+      'btn-lg': this.size === 'lg',
+      'rounded-pill': this.rounded,
+      'w-100': this.block,
+      'btn-icon': this.iconOnly,
+      [this.customClasses]: !!this.customClasses, // permite clases personalizadas externas
+    };
+  }
+}
